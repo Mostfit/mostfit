9023a4ff415e2af91b33944270b793240c984b24
cies <cies@wit.domain>
2009-02-02 16:58:44 +0530

    specified the loan-staff relations from the staff side

--- app/models/staff_member.rb
+++ app/models/staff_member.rb
@@ -6,11 +6,14 @@ class StaffMember
   property :active,  Boolean, :default => true, :nullable => false
   # no designations, they are derived from the relations it has
 
-  has n, :centers
   has n, :branches
+  has n, :centers
+  has n, :approved_loans,    :child_key => [:approved_by_staff_id],    :class_name => 'Loan'
+  has n, :disbursed_loans,   :child_key => [:disbursed_by_staff_id],   :class_name => 'Loan'
+  has n, :written_off_loans, :child_key => [:written_off_by_staff_id], :class_name => 'Loan'
+
 
   validates_is_unique :name
-
 
 end
 
