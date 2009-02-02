dbe150b3dbd92321ac1d7aa74bcd6a17a480452e
cies <cies@wit.domain>
2009-02-02 17:38:42 +0530

    minor refactoring

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,5 +1,7 @@
 class Payment
   include DataMapper::Resource
+
+  attr_writer :total  # just to be used in the form
   
   property :id,             Serial
   property :principal,      Integer, :nullable => false
@@ -11,23 +13,21 @@ class Payment
   belongs_to :loan
   belongs_to :created_by,  :class_name => 'User'
   belongs_to :received_by, :child_key => [:received_by_staff_id], :class_name => 'StaffMember'
-  belongs_to :deleted_by,  :child_key => [:deleted_by_user_id],  :class_name => 'User'
+  belongs_to :deleted_by,  :child_key => [:deleted_by_user_id],   :class_name => 'User'
 
   validates_present :loan_id, :created_by, :received_by
   validates_with_method :properly_deleted?
-  validates_with_method :total, :correct_total?
   # payment principa/interest/total not more then loan
   # received on cannot be in the future
   # received_by has to point an active staff member
+
+  def total
+    principal + interest
+  end
 
   private
   def properly_deleted?
     return true if (self.deleted_by and self.deleted_at) or (!self.deleted_by and !self.deleted_at)
     [false, "deleted_by and deleted_at properties have to be (un)set together"]
-  end
-
-  def correct_total?
-    return true if self.total == self.principal + self.interest
-    [false, "the sum of principal and interest should equal the total"]
   end
 end
