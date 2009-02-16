e5e7367a08f4fece713dd226e166eddde3bd46d1
Siddharth <svs@vincent.(none)>
2009-02-16 15:35:30 +0530

    data_entry_privileges added

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,6 +1,10 @@
 class Application < Merb::Controller
   before :ensure_authenticated
 
+  def ensure_has_data_entry_privileges
+    raise NotPrivileged unless session.user.data_entry_operator? || session.user.mis_manager? || session.user.admin?
+  end
+
   def ensure_has_mis_manager_privileges
     raise NotPrivileged unless session.user.mis_manager? || session.user.admin?
   end
