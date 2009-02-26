1a398a597e6ac3e0578785ac660817e54ca2a4b3
Siddharth <svs@vincent.(none)>
2009-02-26 21:02:23 +0530

    Cleaned up more production/dev conflicts. i.e. sql statement in loan_history model

--- app/models/loan_history.rb
+++ app/models/loan_history.rb
@@ -41,16 +41,16 @@ class LoanHistory
   # i.e. making a graph, for every 'measurment' in the graph (a bunch of date on the x axis)
   # this method is called once.
   def self.sum_outstanding_for(date, loan_ids)
-    repository.adapter.query(%Q{
-      SELECT        -- aggregates history info for multiple loans on a given date
-        SUM ("scheduled_outstanding_principal") AS "scheduled_outstanding_principal",
-        SUM ("scheduled_outstanding_total")     AS "scheduled_outstanding_total",
-        SUM ("actual_outstanding_principal")    AS "actual_outstanding_principal",
-        SUM ("actual_outstanding_total")        AS "actual_outstanding_total"
-       FROM ( SELECT "scheduled_outstanding_principal", "scheduled_outstanding_total",
-                     "actual_outstanding_principal", "actual_outstanding_total", MAX("date") FROM "loan_history"
-               WHERE ("loan_id" IN (#{loan_ids.join(', ')})) AND ("date" <= #{date.to_s.inspect})
-            GROUP BY "loan_id" ) })[0]  # in case of nil errors add: .map { |x| x.nil? ? 0 : x }  # nil -> 0
+    repository.adapter.query("
+      SELECT  
+        SUM(scheduled_outstanding_principal) AS scheduled_outstanding_principal,
+        SUM(scheduled_outstanding_total)     AS scheduled_outstanding_total,
+        SUM(actual_outstanding_principal)    AS actual_outstanding_principal,
+        SUM(actual_outstanding_total)        AS actual_outstanding_total
+       FROM( SELECT scheduled_outstanding_principal, scheduled_outstanding_total,
+                     actual_outstanding_principal, actual_outstanding_total, MAX(date) FROM loan_history
+               WHERE (loan_id IN (#{loan_ids.join(', ')})) AND (date <= #{date.to_s})
+            GROUP BY loan_id ) AS derived_table" )[0]  # in case of nil errors add: .map { |x| x.nil? ? 0 : x }  # nil -> 0
   end
 
 end
