41baf07f93fa938f1dc32d4bf3c66f706ad7499c
piyush <piyush@piyush-laptop.(none)>
2009-07-01 14:45:14 +0530

    added render_to_pdf function in application controller

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -12,8 +12,21 @@ class Application < Merb::Controller
   def ensure_has_admin_privileges
     raise NotPrivileged unless session.user.admin?
   end
-end
 
+  def render_to_pdf(options = nil)
+    data = render_to_string(options)
+    pdf = PDF::HTMLDoc.new
+    pdf.set_option :bodycolor, :white
+    pdf.set_option :toc, false
+    pdf.set_option :portrait, true
+    pdf.set_option :links, false
+    pdf.set_option :webpage, true
+    pdf.set_option :left, '2cm'
+    pdf.set_option :right, '2cm'
+    pdf << data
+    pdf.generate
+  end
+end
 
 
 # small monkey patch, real patch is submitted to extlib/merb/dm, hoping for inclusion soon
