e5b481013940bec01b968bc2f4f45a6f89944fd4
svs <svs@vincent.(none)>
2009-07-13 17:59:03 +0530

    updated gems

--- config/dependencies.rb
+++ config/dependencies.rb
@@ -1,7 +1,7 @@
 # dependencies are generated using a strict version, don't forget to edit the dependency versions when upgrading.
-merb_gems_version = "1.0.11"
+merb_gems_version = "1.0.12"
 dm_gems_version   = "0.9.11"
-do_gems_version   = "0.9.11"
+do_gems_version   = "0.9.12"
 
 # For more information about each component, please read http://wiki.merbivore.com/faqs/merb_components
 dependency "merb-core", merb_gems_version
