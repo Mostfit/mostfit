d57fc4df69e413f08310cfebf6320245df9a644b
svs <svs@vincent.(none)>
2009-10-14 19:28:19 +0530

    small fix

--- app/controllers/loans.rb
+++ app/controllers/loans.rb
@@ -32,7 +32,7 @@ class Loans < Application
   def create
     klass, attrs = get_loan_and_attrs
     debugger
-    attrs[:interest_rate] = attrs[:interest_rate] / 100 if attrs[:interest_rate] > 1
+    attrs[:interest_rate] = attrs[:interest_rate] / 100 if attrs[:interest_rate].to_f > 1
     @loan = klass.new(attrs)
     @loan.client = @client  # set direct context
     if @loan.save
