94f8dcff85613b73d6ce4cb27602c73162db31ce
Piyush Ranjan <piyush.pr@gmail.com>
2009-10-16 03:42:12 +0530

    New files for performance reporting

---
+++ app/models/reports/organization.rb
@@ -0,0 +1,60 @@
+module Reporting
+  module OrganizationReports
+    def query_as_number(sql)
+      repository.adapter.query(sql)[0]
+    end
+
+    def branch_count(end_date)
+      query_as_number(%Q{
+          SELECT COUNT(b.id) as count
+          FROM branches b
+          WHERE  b.created_at < '#{end_date}'})
+    end
+
+    def centers_count(end_date)
+      query_as_number(%Q{
+          SELECT COUNT(c.id) as count
+          FROM centers c
+          WHERE  c.created_at < '#{end_date}'})
+    end
+
+    def cms_count(end_date)
+      query_as_number(%Q{
+          SELECT COUNT(DISTINCT(c.manager_staff_id)) as count
+          FROM centers c
+          WHERE  c.created_at < '#{end_date}'})
+    end
+
+    def clients_count(end_date)
+      query_as_number(%Q{
+          SELECT COUNT(*) as count
+          FROM clients c
+          WHERE c.date_joined < '#{end_date}'})
+    end
+
+    def borrowers(end_date)
+      query_as_number(%Q{
+         SELECT COUNT(DISTINCT client_id)
+         FROM loan_history lh
+         WHERE lh.status <= 3 AND lh.created_at<'#{end_date}'})
+    end
+
+    def disbursed(end_date, what)
+      end_date = Date.parse(end_date) unless end_date.is_a? Date
+      return unless what.downcase == "sum" or "count"
+        query_as_number(%Q{
+         SELECT #{what}(l.amount)
+         FROM loans l
+         WHERE l.disbursal_date < '#{end_date}'
+        })
+    end
+
+    def net_portfolio(end_date)
+      repository.adapter.query(%Q{
+        SELECT min(actual_outstanding_total) tot
+        FROM loan_history where status=3 and date BETWEEN '#{start_date}' AND '#{end_date}'
+        GROUP BY loan_id
+        ORDER BY loan_id, date DESC}).reduce{|sum, x| sum+x}
+    end
+  end
+end
---
+++ app/models/reports/performance_report.rb
@@ -0,0 +1,34 @@
+class PerformanceReport < Report
+  include Reporting::OrganizationReports
+
+  def initialize(start_date)
+    self.start_date = (start_date.is_a? Date) ? start_date : Date.parse(start_date)
+    self.end_date   = self.start_date + (Date.new(start_date.year,12,31).to_date<<(12-start_date.month)).day
+    @name = "Performance report upto #{end_date.strftime("%d-%m-%Y")}"
+  end
+
+  def name
+    "Performance report upto #{end_date.strftime("%d-%m-%Y")}"
+  end
+
+  def to_str
+    "#{start_date} - #{end_date}"
+  end
+
+  def calc
+    @report = []
+    t0 = Time.now
+    puts "generating..."
+    @report[0] = {'Branches' => branch_count(end_date)}
+    @report[1] = {'Centers' => centers_count(end_date)}
+    @report[2] = {"CMs"  => cms_count(end_date)}
+    @report[3] = {"Clients" => clients_count(end_date)}
+    @report[4] = {"Borrowers" => borrowers(end_date)}
+    @report[5] = {"Disbursed" => disbursed(end_date, "sum")}
+    @report[6] = {"Net portfolio" => net_portfolio(end_date)}
+    self.raw = @report
+    self.report = Marshal.dump(@report)
+    self.generation_time = Time.now - t0
+    self.save
+  end
+end
