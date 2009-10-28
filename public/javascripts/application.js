7d4dc3c763942607c0d14fe1994adc5f68761cf1
Piyush Ranjan <piyush.pr@gmail.com>
2009-10-28 18:35:12 +0530

    JS and style changes for upload status

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,1 +1,22 @@
 // Common JavaScript code across your application goes here.
+var lineNos=0;
+function spitLogs(){
+    $.get("/logs/"+$("div.log_box").attr("id"), function(data){
+	    lines = data.split("\n");	    
+	    if(lineNos < lines.length-1){
+		for(i=lineNos; i<lines.length-1; i++){
+		    $("div.log_box").append(lines[i]+'<br/>');	    
+		}
+		lineNos=lines.length-1;
+		$("div.log_box").attr({scrollTop: $("div.log_box").attr("scrollHeight") });
+	    }
+	});
+}
+
+$(document).ready(function(){
+	if($("div.log_box").length>0){
+	    setInterval(function(){
+		    spitLogs();
+		}, 2000);
+	}
+    });
