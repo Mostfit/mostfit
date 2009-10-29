8ae4d4e3e054b34196c5564b751b37525934e02d
Piyush Ranjan <piyush.pr@gmail.com>
2009-10-29 17:45:25 +0530

    Update history to be run as part of upload script

--- app/controllers/admin.rb
+++ app/controllers/admin.rb
@@ -21,8 +21,10 @@ class Admin < Application
       Merb.run_later do        
         log.info("File has been uploaded to the server. Now processing it into a bunch of csv files")
         file.process_excel_to_csv
-        log.info("CSVs extraction complete. Processing files.")
+        log.info("CSV extraction complete. Processing files.")
         file.load_csv(log)
+        log.info("CSV files are now loaded into the DB. Creating loan schedules.")
+        `rake mock:update_history`
         log.info("<h2>Processing complete! Your MIS is now ready for use. Please take a note of all the errors reported here(if any) and rectify them.</h2>")        
       end
       redirect "/admin/upload_status/#{file.directory}"
