86762d1cc4d59cd8dcb5414d1bdac7ea5b3850a2
Piyush Ranjan <piyush.pr@gmail.com>
2010-01-14 02:28:51 +0530

    give verifications rights to only admin

--- config/misfit.rb
+++ config/misfit.rb
@@ -33,7 +33,7 @@ module Misfit
 
     @crud_rights = {
       :admin => all_models,
-      :mis_manager => all_models_except([:user, :admin]),
+      :mis_manager => all_models_except([:user, :admin, :verifications]),
       :data_entry => {
         :all => [:client, :loan, :payment],
       },
@@ -44,7 +44,7 @@ module Misfit
 
     @access_rights = {
       :admin => all_controllers,
-      :mis_manager => all_controllers_except([:users, :admin]),
+      :mis_manager => all_controllers_except([:users, :admin, :verifications]),
       :data_entry => {
         :all => [:"data_entry/payments",:"data_entry/clients",:"data_entry/loans", :"data_entry/index"]
       },
