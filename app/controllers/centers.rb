2bcb48029907ec9f573f432742cdcd7bc1ea6bf2
Piyush Ranjan <piyush.pr@gmail.com>
2010-01-20 18:23:22 +0530

    Fixed notice messages being dragged into urls

--- app/controllers/centers.rb
+++ app/controllers/centers.rb
@@ -54,7 +54,7 @@ class Centers < Application
       @center.branch = @branch  # set direct context
     end
     if @center.save
-      redirect(params[:return]||resource(@branch, :centers), :message => {:notice => "Center '#{@center.name}' successfully created"})
+      redirect(params[:return]||resource(@branch), :message => {:notice => "Center '#{@center.name}' successfully created"})
     else
 #       message[:error] = "Center failed to be created"
       render :new  # error messages will be shown
@@ -72,7 +72,7 @@ class Centers < Application
     @center = Center.get(id)
     raise NotFound unless @center
     if @center.update_attributes(center)
-      redirect(params[:return]||resource(@branch, :centers), :message => {:notice => "Center '#{@center.name}' has been successfully edited"})
+      redirect(params[:return]||resource(@branch), :message => {:notice => "Center '#{@center.name}' has been successfully edited"})
     else
       display @center, :edit  # error messages will be shown
     end
