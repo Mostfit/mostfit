aaa5ecc6efc80aac920cccf668729e9d7693b12a
Piyush Ranjan <piyush.pr@gmail.com>
2010-02-03 19:02:04 +0530

    Fixed consolidated report

--- app/models/reports/consolidated_report.rb
+++ app/models/reports/consolidated_report.rb
@@ -63,10 +63,10 @@ class ConsolidatedReport < Report
       center_id = client.center_id
       next if not centers.key?(center_id)
       branch_id = centers[center_id].branch_id
-      if groups[branch_id][center_id][client.client_group_id]
-        groups[branch_id][center_id][client.client_group_id][3] += p.amount if p.type==:principal 
-        groups[branch_id][center_id][client.client_group_id][4] += p.amount if p.type==:interest
-        groups[branch_id][center_id][client.client_group_id][5] += p.amount if p.type==:fees
+      if groups[branch_id][center_id]
+        groups[branch_id][center_id][client.client_group_id ? client.client_group_id : 0][3] += p.amount if p.type==:principal 
+        groups[branch_id][center_id][client.client_group_id ? client.client_group_id : 0][4] += p.amount if p.type==:interest
+        groups[branch_id][center_id][client.client_group_id ? client.client_group_id : 0][5] += p.amount if p.type==:fees
       end
     }
     #1: Applied on
@@ -75,8 +75,7 @@ class ConsolidatedReport < Report
       center_id = client.center_id
       next if not centers.key?(center_id)
       branch_id = centers[center_id].branch_id
-
-      groups[branch_id][center_id][l.client.client_group_id][0] += l.amount
+      groups[branch_id][center_id][l.client.client_group_id ? l.client_group_id : 0][0] += l.amount
     }
 
     #2: Approved on
@@ -86,7 +85,7 @@ class ConsolidatedReport < Report
       next if not centers.key?(center_id)
       branch_id = centers[center_id].branch_id
 
-      groups[branch_id][center_id][l.client.client_group_id][1] += l.amount
+      groups[branch_id][center_id][l.client.client_group_id ? l.client_group_id : 0][1] += l.amount
     }
 
     #3: Disbursal date
@@ -96,7 +95,7 @@ class ConsolidatedReport < Report
       next if not centers.key?(center_id)
       branch_id = centers[center_id].branch_id
 
-      groups[branch_id][center_id][l.client.client_group_id][2] += l.amount
+      groups[branch_id][center_id][l.client.client_group_id ? l.client.client_group_id : 0][2] += l.amount
     }
     return [groups, centers, branches]
   end
