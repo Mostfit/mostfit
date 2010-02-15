f58489a9ffb9430f47fd8c1626ac8ab5fea7fea5
Siddharth <svs@intellecap.net>
2010-02-15 20:59:50 +0530

    start value bug in targets

--- app/models/target.rb
+++ app/models/target.rb
@@ -10,27 +10,27 @@ class Target
   property :type,          Enum.send('[]', *Types), :nullable => false, :index => true
   property :deadline,      Date, :nullable => false, :index => true
   property :attached_to,   Enum.send('[]', *ValidAttaches), :nullable => false, :index => true
-  property :attached_id,   Integer, :nullable => false, :index => true  
+  property :attached_id,   Integer, :nullable => false, :index => true
   property :created_at,    DateTime, :default => Time.now
   property :present_value, Integer, :nullable => true, :index => true
   property :checked_at,    Date
-  
+
   validates_with_method :attached_id, :check_existance
   validates_with_method :deadline, :future_date
   validates_present :target_value
   validates_with_method :target_value, :target_value_invalid
-  
+
   before :valid?, :set_start_value
 
   def check_existance
     return true if responsible
     return [false, "This #{responsible_class} doesn't exists"]
   end
-  
+
   def target_value_invalid
     return [false, "This target value is not present"] if target_value.nil?
     return [false, "This target cannot be applied to this object"] if start_value.nil?
-    return true if target_value > present_value
+    return true if target_value > start_value
     return [false, "This target value of #{target_value} is less than present value of #{start_value}"] if target_value < start_value
     return [false, "This target value of #{target_value} is equal to present value of #{start_value}"] if target_value == start_value
   end
@@ -47,7 +47,7 @@ class Target
   def responsible_class
     Kernel.const_get(self.attached_to.to_s.camelcase)
   end
-  
+
   def fetch_present_value
     klass  = TypeClasses[Types.index(self.type)]
     method = klass.to_s.snake_case.pluralize.to_sym
@@ -60,7 +60,7 @@ class Target
       responsible.send(method).size
     end
   end
-  
+
   def get_present_value
     if not checked_at or checked_at < Date.today
       self.present_value = fetch_present_value
@@ -69,9 +69,9 @@ class Target
     end
     self.present_value
   end
-  
+
   def set_present_value
-    self.present_value = fetch_present_value   
+    self.present_value = fetch_present_value
   end
 
   def set_start_value
