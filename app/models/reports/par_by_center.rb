dac279f0aa8170a8a70e2c17c285c1177a85ef47
Siddharth <svs@intellecap.net>
2010-04-15 16:14:07 +0530

    par report

--- app/models/reports/par_by_center.rb
+++ app/models/reports/par_by_center.rb
@@ -16,33 +16,35 @@ class ParByCenterReport < Report
   end
 
   def generate
-    debugger
-    loan_ids = repository.adapter.query(%Q{
+    row_ids = repository.adapter.query(%Q{ # these are the loan history lines which represent the last line before @date
       SELECT CONCAT(loan_id,'_',max(date))
       FROM loan_history 
       WHERE date < '#{@date.strftime("%Y-%m-%d")}' and amount_in_default > 0
       GROUP BY loan_id})
-    # these are the loan history lines which represent the last line before @date
-    loan_ids = "'" + loan_ids.join("','") + "'"
-    pars = repository.adapter.query(%Q{
-      SELECT branch_id, center_id, amount_in_default, days_overdue + DATEDIFF(#{@date.strftime("%Y-%m-%d")}, created_at) as late_by
+    
+    loans = Loan.all(:id => row_ids.map{|lid| lid.split("_")[0]})
+    
+    row_ids = "'" + row_ids.join("','") + "'"
+    query = %Q{ # These are the lines from the loan history
+      SELECT loan_id,branch_id, center_id, amount_in_default, days_overdue + DATEDIFF('#{@date.strftime("%Y-%m-%d")}', created_at) as late_by
       FROM loan_history 
-      WHERE CONCAT(loan_id,'_',date) IN (#{loan_ids})
-      ORDER BY branch_id, center_id})
+      WHERE CONCAT(loan_id,'_',date) IN (#{row_ids})
+      ORDER BY branch_id, center_id}
+    pars = repository.adapter.query(query)
+    par_map = pars.map{|p| [p.loan_id, p]}.to_hash
+    debugger
                                     
     r = {}
-    pars.each do |p|
-      if r.has_key?(p.branch_id)
-        if r[p.branch_id].has_key?(p.center_id)
-          r[p.branch_id][p.center_id] << p
-        else
-          r[p.branch_id][p.center_id] = [p]
+    Branch.all.each do |b|
+      r[b] = {}
+      b.centers.each do |c|
+        next if @center and not @center.find{|x| x.id==c.id}        
+        r[b][c] = []
+        loans.select{ |l| l.client.center == c}.each do |l|
+          r[b][c] << [l, par_map[l.id]]
         end
-      else
-        r[p.branch_id] = {p.center_id => [p]}
       end
     end
-    debugger
     return r
   end
 end
