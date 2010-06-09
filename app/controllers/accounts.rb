651742d2e1fa186aaa68a2a2a68981b74aaa6267
ravi <ravi@ravi-laptop.(none)>
2010-06-09 18:31:51 +0530

    Accounting stuff is added

---
+++ app/controllers/accounts.rb
@@ -0,0 +1,58 @@
+class Accounts < Application
+  # provides :xml, :yaml, :js
+
+  def index
+    @accounts = Account.all
+    display @accounts
+  end
+
+  def show(id)
+    @account = Account.get(id)
+    raise NotFound unless @account
+    display @account
+  end
+
+  def new
+    only_provides :html
+    @account = Account.new
+    display @account
+  end
+
+  def edit(id)
+    only_provides :html
+    @account = Account.get(id)
+    raise NotFound unless @account
+    display @account
+  end
+
+  def create(account)
+    @account = Account.new(account)
+    if @account.save
+      redirect resource(@account), :message => {:notice => "Account was successfully created"}
+    else
+      message[:error] = "Account failed to be created"
+      render :new
+    end
+  end
+
+  def update(id, account)
+    @account = Account.get(id)
+    raise NotFound unless @account
+    if @account.update(account)
+       redirect resource(@account)
+    else
+      display @account, :edit
+    end
+  end
+
+  def destroy(id)
+    @account = Account.get(id)
+    raise NotFound unless @account
+    if @account.destroy
+      redirect resource(:accounts)
+    else
+      raise InternalServerError
+    end
+  end
+
+end # Accounts
