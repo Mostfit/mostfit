be1012d1d41f976757249c76a78736a4ae84b42e
ravi <ravi@ravi-laptop.(none)>
2010-06-14 18:11:01 +0530

    Accounting modified

--- app/models/account.rb
+++ app/models/account.rb
@@ -4,10 +4,10 @@ class Account
   property :id,                Serial  
   property :name,              String
   property :gl_code,           String
-  property :parent_gl_id,      String
+  property :parent_account_id,      String
   property :account_type,      Enum.send('[]', *TYPES), :nullable => false , :default => :none, :index => true
 
-  #belongs_to :account, :model => 'Account', :child_key => 'parent_id'
+  belongs_to :account, :model => 'Account', :child_key => [:parent_account_id]
 
 def self.account_types
  TYPES
