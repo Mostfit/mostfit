b72f86ee3a424ba8af76b624120eb5340f8e671e
ravi <ravi@ravi-laptop.(none)>
2010-06-18 18:22:53 +0530

    Rule book added

---
+++ app/controllers/rule_books.rb
@@ -0,0 +1,58 @@
+class RuleBooks < Application
+  # provides :xml, :yaml, :js
+
+  def index
+    @rule_books = RuleBook.all
+    display @rule_books
+  end
+
+  def show(id)
+    @rule_book = RuleBook.get(id)
+    raise NotFound unless @rule_book
+    display @rule_book
+  end
+
+  def new
+    only_provides :html
+    @rule_book = RuleBook.new
+    display @rule_book
+  end
+
+  def edit(id)
+    only_provides :html
+    @rule_book = RuleBook.get(id)
+    raise NotFound unless @rule_book
+    display @rule_book
+  end
+
+  def create(rule_book)
+    @rule_book = RuleBook.new(rule_book)
+    if @rule_book.save
+      redirect resource(@rule_book), :message => {:notice => "RuleBook was successfully created"}
+    else
+      message[:error] = "RuleBook failed to be created"
+      render :new
+    end
+  end
+
+  def update(id, rule_book)
+    @rule_book = RuleBook.get(id)
+    raise NotFound unless @rule_book
+    if @rule_book.update(rule_book)
+       redirect resource(@rule_book)
+    else
+      display @rule_book, :edit
+    end
+  end
+
+  def destroy(id)
+    @rule_book = RuleBook.get(id)
+    raise NotFound unless @rule_book
+    if @rule_book.destroy
+      redirect resource(:rule_books)
+    else
+      raise InternalServerError
+    end
+  end
+
+end # RuleBooks
--- app/models/account_payment_observer.rb
+++ app/models/account_payment_observer.rb
@@ -2,16 +2,15 @@ class AccountPaymentObserver
   include DataMapper::Observer
   observe Payment
   
-  def self.make_posting_entries(obj, type)
-  # This function will make entries to the posting database when save, update or delete envent triggers  
+  def self.make_posting_entries(obj)
+  # This function will make entries to the posting database when save and/or delete envent triggers  
     if obj
       attributes = obj.attributes
       Journal.transaction do |t|       
         begin
           credit_account, debit_account = RuleBook.get_accounts(obj)
           journal = Journal.new(:comment => "Payment: #{obj.payment_type} - #{obj.amount}", 
-                                :date => obj.received_on
-                                :transaction_id => obj.id, :created_at => Time.now)
+                                :date => obj.received_on, :transaction_id => obj.id, :created_at => Time.now)
           journal.save      
           
           post = Posting.new(:amount => obj.amount * -1, :journal_id => journal.id, :account => debit_account,
@@ -28,15 +27,39 @@ class AccountPaymentObserver
       end
     end
   end
+
+
+  def self.reverse_posting_entries(obj)
+
+    if obj
+      attributes = obj.attributes
+
+      Journal.transaction do |t|       
+        begin
+          credit_account, debit_account = RuleBook.get_accounts(obj)
+          journal = Journal.new(:comment => "Payment Deleted: #{obj.payment_type} - #{obj.amount}",
+                                :date => obj.deleted_at,:transaction_id => obj.id, :created_at => Time.now)
+          journal.save      
+          
+          post = Posting.new(:amount => obj.amount, :journal_id => journal.id, :account => credit_account,
+                             :currency_id => 1)
+          post.save
+          
+          
+          post = Posting.new(:amount => obj.amount * -1 , :journal_id => journal.id, :account => debit_account,
+                             :currency_id => 1)
+          post.save
+        rescue
+          t.rollback
+        end
+      end
+    end
+  end 
  
   after :create do
    AccountPaymentObserver.make_posting_entries(self)
   end  
     
-  before :destroy do
-    AccountPaymentObserver.get_object_state(self, :destroy) if not self.new?
-  end
-
   after :destroy do
     AccountPaymentObserver.reverse_posting_entries(self)
   end
