3112a52cd391897e5e1ab801cdc924b78cd0891d
Gazal Garg <gazal24@gmail.com>
2010-06-18 19:11:25 +0530

    observer for Payment updated

--- app/models/account_payment_observer.rb
+++ app/models/account_payment_observer.rb
@@ -2,27 +2,29 @@ class AccountPaymentObserver
   include DataMapper::Observer
   observe Payment
   
-  def self.make_posting_entries(obj, type)
-  # This function will make entries to the posting database when save, update or delete envent triggers  
+  def self.make_posting_entries(obj)
+    # This function will make entries to the posting database when save, update or delete envent triggers  
     if obj
       attributes = obj.attributes
       Journal.transaction do |t|       
-        begin
-          credit_account, debit_account = RuleBook.get_accounts(obj)
-          journal = Journal.new(:comment => "Payment: #{obj.payment_type} - #{obj.amount}", 
-                                :date => obj.received_on
-                                :transaction_id => obj.id, :created_at => Time.now)
-          journal.save      
-          
-          post = Posting.new(:amount => obj.amount * -1, :journal_id => journal.id, :account => debit_account,
-                             :currency_id => 1)
-          post.save
-          
-         
-          post = Posting.new(:amount => obj.amount, :journal_id => journal.id, :account => credit_account,
-                             :currency_id => 1)
-          post.save
-        rescue
+        credit_account, debit_account = RuleBook.get_accounts(obj)
+
+        journal = Journal.new(:comment => "Payment: #{obj.type} - #{obj.amount}", 
+                              :transaction_id => obj.id, :created_at => Time.now)
+        journal_saved  = journal.save      
+        
+        
+        post = Posting.new(:amount => obj.amount * -1, :journal_id => journal.id, :account => debit_account,
+                           :currency_id => 1)
+        debit_saved = post.save
+        
+        
+        post = Posting.new(:amount => obj.amount, :journal_id => journal.id, :account => credit_account,
+                           :currency_id => 1)
+        credit_saved = post.save
+        
+        # Rollback in case of any failure above.
+        if not (credit_saved and debit_saved and journal_saved)
           t.rollback
         end
       end
