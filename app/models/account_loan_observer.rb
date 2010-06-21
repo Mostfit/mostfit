2d485f51b89d3e7c8efbf6accd397d261215c4ed
Gazal Garg <gazal24@gmail.com>
2010-06-21 15:02:15 +0530

    observer for loan(update only) added

--- app/models/account_loan_observer.rb
+++ app/models/account_loan_observer.rb
@@ -1,90 +1,42 @@
 class AccountLoanObserver
-  include DataMapper::Observer    
+  include DataMapper::Observer
   observe Loan
   
 
-
   def self.make_posting_entries_on_update(obj)
-    # This function will make entries to the posting database when save, update or delete event triggers  
-    return false unless obj
+    # This function will make entries to the posting database when save, update or delete envent triggers  
+    if obj
+      if obj.disbursal_date and obj.approved_on
+        Journal.transaction do |t|       
+          credit_account, debit_account = RuleBook.get_accounts(obj)
 
-    original_attributes = obj.original_attributes.map{|k,v| {k.name => v}}.inject({}){|s,x| s+=x}
-    attributes = obj.attributes
-    if    original_attributes[:disbursal_date].nil? and attributes[:disbursal_date].nil?
-      return
-    elsif original_attributes[:disbursal_date].nil? and attributes[:disbursal_date]
-      self.forward_entry(obj)
-    elsif original_attributes[:disbursal_date]      and attributes[:disbursal_date].nil?
-      self.reverse_entry(obj)
-    elsif attributes[:disbursal_date] != original_attributes[:disbursal_date]
-      self.reverse_entry(obj)
-      self.forward_entry(obj)
+          journal = Journal.new(:comment => "Loan: #{obj.discriminator} - #{obj.amount}", 
+                                :transaction_id => obj.id, :created_at => Time.now)
+          journal_saved  = journal.save      
+          
+          
+          post = Posting.new(:amount => obj.amount * -1, :journal_id => journal.id, :account => debit_account,
+                             :currency_id => 1)
+          debit_saved = post.save
+          
+          
+          post = Posting.new(:amount => obj.amount, :journal_id => journal.id, :account => credit_account,
+                             :currency_id => 1)
+          credit_saved = post.save
+          
+          # Rollback in case of any failure above.
+          if not (credit_saved and debit_saved and journal_saved)
+            t.rollback
+          end
+        end
+      end
     end
+  end
     
-    if original_attributes[:amount] != attributes[:amount]
-      
-    end
-  end
-  
-  def self.forward_entry(obj)
-    Journal.transaction do |t|       
-      credit_account, debit_account = RuleBook.get_accounts(obj)
-      
-      journal = Journal.new(:comment => "Loan: #{obj.client.name} - #{obj.amount}", 
-                            :date => obj.disbursal_date, :transaction_id => obj.id, :created_at => Time.now)
-      journal_saved  = journal.save      
-      
-      
-      post = Posting.new(:amount => obj.amount * -1, :journal_id => journal.id, :account => debit_account,
-                         :currency_id => 1)
-      debit_saved = post.save
-      
-      
-      post = Posting.new(:amount => obj.amount, :journal_id => journal.id, :account => credit_account,
-                         :currency_id => 1)
-      credit_saved = post.save
-      
-      # Rollback in case of any failure above.
-      if not (credit_saved and debit_saved and journal_saved)
-        t.rollback
-      end
-    end
-  end
-  
-  
-  def self.reverse_entry(obj)
-    Journal.transaction do |t|       
-      credit_account, debit_account = RuleBook.get_accounts(obj)
-      
-      journal = Journal.new(:comment => "Loan: #{obj.client.name} - #{obj.amount} - reverse entry", 
-                            :date => obj.disbursal_date, :transaction_id => obj.id, :created_at => Time.now)
-      journal_saved  = journal.save      
-      
-      
-      post = Posting.new(:amount => obj.amount, :journal_id => journal.id, :account => debit_account,
-                         :currency_id => 1)
-      debit_saved = post.save
-      
-      
-      post = Posting.new(:amount => obj.amount * -1, :journal_id => journal.id, :account => credit_account,
-                         :currency_id => 1)
-      credit_saved = post.save
-      
-      # Rollback in case of any failure above.
-      if not (credit_saved and debit_saved and journal_saved)
-        t.rollback
-      end
-    end
-  end
 
-  
-  before :update do
+  after :update do
     AccountLoanObserver.make_posting_entries_on_update(self)
   end  
-  
-  before :destroy do
-    AccountLoanObserver.get_object_state(self, :destroy) if not self.new?
-  end
   
   after :destroy do
     AccountLoanObserver.reverse_posting_entries(self)
