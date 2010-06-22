2db91077c5a27b9ba38e4fa9fce8384b585b3062
ravi <ravi@ravi-laptop.(none)>
2010-06-22 19:55:43 +0530

    Trial balance is added

--- app/models/reports/trial_balance_report.rb
+++ app/models/reports/trial_balance_report.rb
@@ -1,5 +1,5 @@
 class TrialBalanceReport < Report
-  attr_accessor :from_date, :to_date, :account, :account_id, :journal,:posting, :account_type_id
+  attr_accessor :from_date, :to_date, :account, :account_id, :journal,:posting
 
   def initialize(params,dates, user)
     @from_date = (dates and dates[:from_date]) ? dates[:from_date] : Date.min_date
@@ -17,8 +17,7 @@ class TrialBalanceReport < Report
   end
 
   def generate
-  
-    Account.all(:order => [:account_type_id.asc])
+    Account.all
   end
 end   
 
