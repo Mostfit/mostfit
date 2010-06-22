53b38d143a08edcc655ffc01bdea76be3cda2edd
Gazal Garg <gazal24@gmail.com>
2010-06-22 16:39:30 +0530

    Date field added in Journal and in PaymentObserver

--- app/models/journal.rb
+++ app/models/journal.rb
@@ -4,6 +4,7 @@ class Journal
   property :id,             Serial
   property :comment,        String 
   property :transaction_id, Integer
+  property :date,           Date
   property :created_at,     DateTime
   property :batch_id,       Integer, :nullable => true
 
