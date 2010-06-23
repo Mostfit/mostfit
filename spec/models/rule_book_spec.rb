f84712e90794ab0281c4be0ca035b74e27776322
ravi <ravi@ravi-laptop.(none)>
2010-06-23 16:09:46 +0530

    spec rule book added

--- spec/models/rule_book_spec.rb
+++ spec/models/rule_book_spec.rb
@@ -3,19 +3,19 @@ require File.join( File.dirname(__FILE__), '..', "spec_helper" )
 describe RuleBook do
  
   before (:all) do
+#    @user = User.new(:login => 'admin', :password => 'password', :password_confirmation => 'password',
+#                     :role => :admin)
+#    @user.save
+#    @user.should be_valid
+    
     @account_type = AccountType.new(:name => 'Assets',:code => '20000')
     @account_type.save
     @account_type.should be_valid 
-    
-    @credit_account = Account.new(:name => 'petty cash', :opening_balance => '0', :gl_code => '10001',
-                           :account_type => @account_type)
-    @credit_account.save
-    @credit_account.should be_valid
-
-    @debit_account = Account.new(:name => 'income account', :opening_balance => '0', :gl_code => '20002',
-                           :account_type => @account_type)
-    @debit_account.save
-    @debit_account.should be_valid
+   
+    @account = Account.new(:name => 'petty cash', :opening_balance => '0', :gl_code => '10001',
+                        :parent_id => @account, :account_type => @account_type)
+    @account.save
+    @account.should be_valid
     
     @manager = StaffMember.new(:name => 'Mr Ravan')
     @manager.save
@@ -28,21 +28,20 @@ describe RuleBook do
   end
 
   before (:each) do
-    @rule_book = RuleBook.new(:name => "principal repayment", :action => 'principal',
-                              :credit_account => @credit_account ,:debit_account => @debit_account,:branch =>@branch)
+    @rule_book = RuleBook.new(:name => 'principal repayment', :action => 'principal',
+                              :credit_account =>@account ,:debit_account =>@account,:branch =>@branch )
     @rule_book.save
-    @rule_book.errors
     @rule_book.should be_valid 
-  end
 
-  it "should not be valid if name is less than 3 character" do
+  end
+  it "name should not be less than 3 charactor" do
     @rule_book.name = "xz"
     @rule_book.should_not be_valid 
   end
-  
-  it "should not be valid if credit account and debit account is same" do
-    @rule_book.credit_account = @credit_account
-    @rule_book.debit_account  = @credit_account
+
+  it "credit account & debit account should not be same" do
+    @rule_book.credit_account = @account
+    @rule_book.debit_account = @account
     @rule_book.should_not be_valid
   end
   
