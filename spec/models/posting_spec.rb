60c6c4f5aca783d42ac28bc655502ca3de6edbc8
Gazal Garg <gazal24@gmail.com>
2010-06-23 17:37:33 +0530

    Posting and Rule Book spec updated

--- spec/models/posting_spec.rb
+++ spec/models/posting_spec.rb
@@ -1,14 +1,45 @@
 require File.join( File.dirname(__FILE__), '..', "spec_helper" )
 
 describe Posting do
-
   before (:all) do
+    @account_type = AccountType.new()
+    @account_type.save
+    @account_type.error
+    @account_type.should be_valid
     
-    @user = User.new(:login => 'admin', :password => 'password', :password_confirmation => 'password',
-                     :role => :admin)
-    @user.save
-    @user.should be_valid 
-
-  it "should have specs"
-
+    @journal  = Journal.new(:comment => "A principal is repayed", :transaction_id => 100, :date => Date.today, :created_at => Date.today)
+    @journal.save
+    @journal.error
+    @journal.should be_valid
+    
+    @account  = Account.new(:name => "Income Account", :gl_code => "IA1002", :account_type => @account_type)
+    @account.save
+    @account.error
+    @account.should be_valid
+    
+    @currency = Currency.new(:name => "rupee")
+    @currency.save
+    @currency.error
+    @currency.should be_valid
+  end
+  
+  before (:each) do
+    Posting.all.destroy!
+    @posting = Posting.new(:amount => 0, :journal => @journal, :account => @account, :currency => @currency) 
+  end
+  
+  it "should contain a journal" do
+    @posting.journal = nil
+    @posting.should_not be_valid
+  end
+  
+  it "should contain an account" do
+    @posting.account = nil
+    @posting.should_not be_valid
+  end
+  
+  it "should contain a currency" do
+    @posting.currency = nil
+    @posting.should_not be_valid
+  end
 end
