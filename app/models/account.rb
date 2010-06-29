23712ffdb511d1221f53eb96cac12a8458564f92
Gazal Garg <gazal24@gmail.com>
2010-06-29 12:13:21 +0530

    minor improvements in accounting

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,25 +1,36 @@
 class Account
   include DataMapper::Resource
+  before :save, :convert_blank_to_nil
 
-  property :id,                     Serial  
-  property :name,                   String
-  property :opening_balance,        Integer, :default => 0 
-  property :opening_balance_on_date, Date, :nullable => false, :default => Date.today
-  property :gl_code,                String
-  property :parent_id,              String
-  belongs_to :account, :model => 'Account', :child_key => [:parent_id]
+  property   :id,                      Serial  
+  property   :name,                    String
+  property   :opening_balance,         Integer, :default => 0 
+  property   :opening_balance_on_date, Date, :nullable => false, :default => Date.today
+  property   :gl_code,                 String
+  property   :parent_id,               String
+  belongs_to :account,                 :model => 'Account', :child_key => [:parent_id]
   belongs_to :account_type
-   
+  
+  property   :branch_id,               Integer, :nullable => true, :index => true
+  belongs_to :branch,                  :model => 'Branch', :child_key => [:branch_id]
+
   has n, :postings
   is :tree, :order => :name
 
-  validates_present :name 
-  validates_present :gl_code
-  validates_length  :name,     :minimum => 3
-  validates_length  :gl_code,  :minimum => 3  
+  validates_present   :name 
+  validates_present   :gl_code
+  validates_length    :name,     :minimum => 3
+  validates_length    :gl_code,  :minimum => 3  
+  validates_is_unique :name
   validates_is_unique :gl_code
   validates_is_number :opening_balance
+
+  
+  def convert_blank_to_nil
+    self.attributes.each{|k, v|
+      if v.is_a?(String) and v.empty? and self.class.send(k).type==Integer
+        self.send("#{k}=", nil)
+      end
+    }
+  end
 end
-
-
-
--- spec/models/rule_book_spec.rb
+++ spec/models/rule_book_spec.rb
@@ -1,19 +1,20 @@
 require File.join( File.dirname(__FILE__), '..', "spec_helper" )
 
 describe RuleBook do
- 
   before (:all) do
-    @account_type = AccountType.new(:name => 'Assets',:code => '20000')
+    Branch.all.destroy!
+
+    @account_type = AccountType.new(:name => 'Assets',:code => 'AST')
     @account_type.save
     @account_type.should be_valid 
     
     @credit_account = Account.new(:name => 'petty cash', :opening_balance => '0', :gl_code => '10001',
-                           :account_type => @account_type)
+                           :account_type => @account_type, :opening_balance_on_date => Date.today)
     @credit_account.save
     @credit_account.should be_valid
 
     @debit_account = Account.new(:name => 'income account', :opening_balance => '0', :gl_code => '20002',
-                           :account_type => @account_type)
+                           :account_type => @account_type,  :opening_balance_on_date => Date.today) 
     @debit_account.save
     @debit_account.should be_valid
     
@@ -26,15 +27,16 @@ describe RuleBook do
     @branch.save
     @branch.should be_valid
   end
-
+  
   before (:each) do
-    @rule_book = RuleBook.new(:name => "principal repayment", :action => 'principal',
-                              :credit_account => @credit_account ,:debit_account => @debit_account,:branch =>@branch)
+    RuleBook.all.destroy!
+    @rule_book = RuleBook.new(:name => "principal repayment", :action => 'principal', 
+                              :credit_account => @credit_account, :debit_account => @debit_account, :branch => @branch)
+    @rule_book.should be_valid
     @rule_book.save
     @rule_book.errors
-    @rule_book.should be_valid 
   end
-
+  
   it "should not be valid if name is less than 3 character" do
     @rule_book.name = "xz"
     @rule_book.should_not be_valid 
@@ -46,4 +48,12 @@ describe RuleBook do
     @rule_book.should_not be_valid
   end
   
+  it "should not be valid if same action exist twice for a branch" do
+    @rule_book_2 = RuleBook.new(:name => "principal repayment", :action => 'principal',
+                              :credit_account => @credit_account ,:debit_account => @debit_account,:branch =>@branch)
+    @rule_book_2.save
+    @rule_book_2.errors
+    @rule_book_2.should_not be_valid 
+  end
+
 end
