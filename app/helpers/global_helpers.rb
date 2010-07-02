d7331c2768f69c8e7d547ca9ee734f1acee02137
Piyush Ranjan <piyush.pr@gmail.com>
2010-07-02 15:31:48 +0530

    show only two digits

--- app/helpers/global_helpers.rb
+++ app/helpers/global_helpers.rb
@@ -176,7 +176,7 @@ module Merb
 
     def format_currency(i)
       # in case of our rupees we do not count with cents, if you want to have cents do that here
-      i.round(4).to_s + " INR"
+      i.round(2).to_s + " INR"
     end
 
     def plurial_nouns(freq)
