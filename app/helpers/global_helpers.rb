2d21f102db69164b43595364d15bedcb43ad2f97
Ashish Bhatia <ashishbhatia.ab@gmail.com>
2010-08-30 14:37:22 +0530

    Increased the size of date field (minor change)

--- app/helpers/global_helpers.rb
+++ app/helpers/global_helpers.rb
@@ -114,7 +114,7 @@ module Merb
 
     def date_select_html (attrs, obj = nil, col = nil)
       str = %Q{
-        <input type='text' name="#{attrs[:name]}" id="#{attrs[:id]}" value="#{attrs[:date]}" size="12">
+        <input type='text' name="#{attrs[:name]}" id="#{attrs[:id]}" value="#{attrs[:date]}" size="20">
         <script type="text/javascript">
           $(function(){
             $("##{attrs[:id]}").datepicker('destroy').datepicker({altField: '##{attrs[:id]}', buttonImage: "/images/calendar.png", changeYear: true, buttonImageOnly: true,
