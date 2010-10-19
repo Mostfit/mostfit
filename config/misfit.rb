e585cbe34e11ebdf5178cd79bc072a2c74ca1cfe
Piyush Ranjan <piyush.pr@gmail.com>
2010-10-19 17:11:42 +0530

    fixed read only user attendance access

--- config/misfit.rb
+++ config/misfit.rb
@@ -54,7 +54,7 @@ module Misfit
       },
       :read_only => {
         :all => [:searches, :browse, :branches, :centers, :payments, :clients, :loans, :dashboard, :regions, :reports, :documents, :comments, :insurance_policies, 
-                 :insurance_companies, :areas, :staff_members, :loan_products, :holidays, :document_types, :occupations, :client_types, :fees, :funders]
+                 :insurance_companies, :areas, :staff_members, :loan_products, :holidays, :document_types, :occupations, :client_types, :fees, :funders, :attendances]
       },
       :staff_member => {
         :all => [:documents, :searches, :browse, :branches, :centers, :payments, :clients, :client_groups, :groups, :audit_trails, :comments, :insurance_policies, 
