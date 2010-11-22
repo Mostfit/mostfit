5d929468c508f05dbfdb0507a10a2acb9e8c7e4a
Ravi Shankar <ravioshankar@gmail.com>
2010-11-22 13:13:15 +0530

    incentive report change

--- app/models/reports/incentive_report.rb
+++ app/models/reports/incentive_report.rb
@@ -14,20 +14,19 @@ class IncentiveReport < Report
 
   def generate
        
-  
     data, @hand_over_w,@taken_over_w,@hand_over_m,@taken_over_m = {},{},{},{},{}
     StaffMember.all.to_hash
-    @from_date = Date.new(Date.today.year,Date.today.month,1) - 600
-    @to_date = Date.new(Date.today.year,Date.today.month, -1) 
-#    @from_date = '2009.1.1'
-#    @to_date = '2010.12.12'
+#    @from_date = Date.new(Date.today.year,Date.today.month,1) 
+ #   @to_date = Date.new(Date.today.year,Date.today.month, -1) 
+    @from_date = '2009.1.1'
+    @to_date = '2010.12.12'
     @from_date_last = Date.new(Date.today.year,Date.today.month-1,1)
     @to_date_last = Date.new(Date.today.year,Date.today.month-1, -1)
     
     
     @net_mgt_w,@net_mgt_m,@d2,@d4,@d15,@d17 = 0,0,0,0,0,0
   
-    StaffMember.all(:active => true).each_with_index{ |sm, idx|
+    StaffMember.all(:active => true,:order => [:id.desc]).each_with_index{ |sm, idx|
       data[sm]||={}
       abc = AuditTrail.all(:auditable_type => Center, :action => :update)
       
@@ -57,7 +56,7 @@ class IncentiveReport < Report
                   @hand_over_w[@staff2] = loan_count
                 @taken_over_w.map{ |k,v|     
                   if @staff1 == k
-                    @taken_over_w[@staff1] = v 
+                    @taken_over_w[@staff1] = v
                   else
                     @taken_over_w[@staff1] = loan_count
                   end
@@ -83,7 +82,7 @@ class IncentiveReport < Report
       
     }
    
-    StaffMember.all(:active => true).each_with_index{ |sm, idx|
+    StaffMember.all(:active => true,:order => [:id.desc]).each_with_index{ |sm, idx|
       data[sm]||={}
    
       data[sm][0] = idx+1
@@ -102,18 +101,21 @@ class IncentiveReport < Report
        
         @taken_over_w.map{|k,v| 
           if (k == sm.id)
-            @d2 += v[0]
-            data[sm][8] = @d2
+            @d2 = v[0]
+           data[sm][8] = @d2
+            
           end
+
         }
         
         d3 = repository.adapter.query("select count(*) from clients cl,centers c, staff_members sm where cl.date_joined between '#{@from_date_last}' and '#{@to_date_last}' and cl.center_id = c.id and cl.deleted_at is NULL and c.manager_staff_id = sm.id and sm.id = #{sm.id}")[0]
 
         @hand_over_w.map{|k,v|
           if(k == sm.id)
-            @d4 += v[0]
+            @d4 = v[0]
             data[sm][10] = @d4
           end
+         
         }
         
         d5 = repository.adapter.query("select count(distinct (l.id)) from loans l ,loan_history lh,staff_members sm where l.id = lh.loan_id and l.installment_frequency = #{lf} and lh.status = 7 and l.disbursed_by_staff_id = #{sm.id} and l.deleted_at is NULL and lh.date between '#{@from_date}' and '#{@to_date}'")[0]
@@ -122,14 +124,13 @@ class IncentiveReport < Report
 
         @taken_over_m.map{|k,v| 
           if (k == sm.id)
-            @d15 += v[0]
+            @d15 = v[0]
             data[sm][15] = @d15
           end
         }
-        
         @hand_over_m.map{|k,v|
           if(k == sm.id)
-            @d17 += v[0]
+            @d17 = v[0]
             data[sm][17] = @d17
           end
         }
@@ -139,8 +140,7 @@ class IncentiveReport < Report
           data[sm][11] = d5
           data[sm][12] = d6
          
-          data[sm][13] = data[sm][22] = ((d1[0]+d3[0]+@d2[0]) - (d5[0]+d6[0]+@d4[0]))
-         
+          data[sm][13] = data[sm][22] = ((d1[0]+d3[0]+@d2[0]) - (d5[0]+d6[0]+@d4[0]))  
         else
           data[sm][14] = d1
           data[sm][16] = d3
