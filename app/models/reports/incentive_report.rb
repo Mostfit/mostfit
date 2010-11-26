0b79d04cfce282c5ee8dd53b131159eabee4b1e9
Ravi Shankar <ravioshankar@gmail.com>
2010-11-26 14:22:56 +0530

    ohhh last cut over incentive report

--- app/models/reports/incentive_report.rb
+++ app/models/reports/incentive_report.rb
@@ -2,7 +2,7 @@ class IncentiveReport < Report
 #  attr_accessor :from_date, :to_date
   def initialize(start_date)  
     self.start_date = (start_date.is_a? Date) ? start_date : Date.parse(start_date)
-    self.end_date   = Date.new(Date.today.year,Date.today.month, -1).strftime('%Y-%m-%d')
+    self.end_date   = Date.new(self.start_date.year,self.start_date.month, -1).strftime('%Y-%m-%d')
     @name = "Incentive report"
   end
  
@@ -27,7 +27,7 @@ class IncentiveReport < Report
   def staff_member_work_area(sm)
     staff_area = repository.adapter.query(%Q{
                            SELECT distinct (b.name)
-                           FROM clients cl, centers c, staff_members sm,branches b
+                           FROM clients cl, centers c, staff_members sm, branches b
                            WHERE cl.center_id = c.id AND c.branch_id = b.id
                                  AND c.manager_staff_id = sm.id AND sm.id = #{sm.id}})
   end
@@ -35,31 +35,31 @@ class IncentiveReport < Report
   def new_client_count_current_month(sm, from_date, to_date)
     new_client_count = repository.adapter.query(%Q{
                             SELECT count(*)
-                            FROM clients cl,centers c, staff_members sm
+                            FROM clients cl, centers c, staff_members sm
                             WHERE cl.date_joined BETWEEN '#{from_date}' AND '#{to_date}'
                                   AND cl.center_id = c.id AND cl.deleted_at is NULL
                                   AND c.manager_staff_id = sm.id AND sm.id = #{sm.id}})
   end
 
-  def new_loan_count_current_month(sm,loan_frequency,from_date, to_date)
+  def new_loan_count_current_month(sm, loan_frequency, from_date, to_date)
     new_loan_count = repository.adapter.query(%Q{
                            SELECT count(distinct(l.id))
-                           FROM loans l,staff_members sm
+                           FROM loans l, staff_members sm
                            WHERE l.disbursal_date BETWEEN '#{from_date}' AND '#{to_date}'
                                  AND l.installment_frequency = #{loan_frequency}
                                  AND l.disbursed_by_staff_id = #{sm.id} AND l.deleted_at is NULL})
   end
 
-  def closed_loan_count_current_month(sm,loan_frequency,from_date, to_date)
+  def closed_loan_count_current_month(sm, loan_frequency, from_date, to_date)
     closed_loan_count = repository.adapter.query(%Q{
                            SELECT count(distinct (l.id))
-                           FROM loans l ,loan_history lh,staff_members sm
+                           FROM loans l, loan_history lh, staff_members sm
                            WHERE l.id = lh.loan_id AND l.installment_frequency = #{loan_frequency}
                               AND lh.status = 7 AND l.disbursed_by_staff_id = #{sm.id}
                               AND l.deleted_at is NULL AND lh.date between '#{from_date}' AND '#{to_date}'})
   end
 
-  def death_cases_current_month(sm,loan_frequency,from_date, to_date)
+  def death_cases_current_month(sm, loan_frequency, from_date, to_date)
     death_count = repository.adapter.query(%Q{
                            SELECT count(*)
                            FROM clients cl,centers c, staff_members sm, loans l
@@ -81,8 +81,8 @@ class IncentiveReport < Report
                                      AND c.manager_staff_id = sm.id AND sm.id = #{sm.id}})
   end
 
-  def center_change_details_for_cm(from_date,to_date)
-    @report,@hand_over_w,@taken_over_w,@hand_over_m,@taken_over_m = {},{},{},{},{}
+  def center_change_details_for_cm(from_date, to_date)
+    @report, @hand_over_w, @taken_over_w, @hand_over_m, @taken_over_m = {},{},{},{},{}
     StaffMember.all(:active => true,:order => [:id.desc]).each{ |sm|
       @report[sm]||={}
         
@@ -149,19 +149,19 @@ class IncentiveReport < Report
   def calc
     t0 = Time.now
     @centers =  Center.all.map{|c| [c.id, c]}.to_hash
-    from_date = Date.new(Date.today.year,Date.today.month,1).strftime('%Y-%m-%d') 
-    to_date = Date.new(Date.today.year,Date.today.month, -1).strftime('%Y-%m-%d') 
+    from_date = self.start_date.strftime('%Y-%m-%d') 
+    to_date = self.end_date.strftime('%Y-%m-%d')  
 
-    from_date_last = Date.new(Date.today.year,Date.today.month-1,1).strftime('%Y-%m-%d')
-    to_date_last = Date.new(Date.today.year,Date.today.month-1, -1).strftime('%Y-%m-%d')
+    from_date_last = Date.new(Date.parse(from_date).year, (Date.parse(from_date).month) - 1, 1).strftime('%Y-%m-%d')  
+    to_date_last = Date.new(Date.parse(to_date).year, (Date.parse(to_date).month) -1, -1).strftime('%Y-%m-%d')
     
-    d2,d4,d15,d17 = 0,0,0,0
+    taken_over_loan_weekly, hand_over_loan_weekly, taken_over_loan_monthly, hand_over_loan_monthly = 0,0,0,0
     
-    center_change_details_for_cm(from_date,to_date)
-   
+    center_change_details_for_cm(from_date, to_date)
+    
     StaffMember.all(:active => true,:order => [:id.desc]).each{ |sm|
       @report[sm][4] = staff_member_work_area(sm)
-      @report[sm][6]= @report[sm][21] = new_client_count_current_month(sm,from_date,to_date)
+      @report[sm][6]= @report[sm][21] = new_client_count_current_month(sm, from_date, to_date)
           
       1.upto(2){ |x|
         if x == 1 
@@ -170,52 +170,52 @@ class IncentiveReport < Report
           loan_frequency = INSTALLMENT_FREQUENCIES.index(:monthly)+1
         end
 
-        d1 = new_loan_count_current_month(sm,loan_frequency,from_date, to_date)
+        loan_count_per_cm_by_frequency = new_loan_count_current_month(sm, loan_frequency, from_date, to_date)
         @taken_over_w.map{|k,v| 
           if (k == sm.id)
-            d2 = v
-            @report[sm][8] = d2
+            taken_over_loan_weekly = v
+            @report[sm][8] = taken_over_loan_weekly
           end
         }
         
-        d3 = previous_month_client_count_by_loan_frequency(sm,loan_frequency,from_date_last,to_date_last)
+        pre_month_loan_count_per_cm_by_frequency = previous_month_client_count_by_loan_frequency(sm, loan_frequency, from_date_last, to_date_last)
         @hand_over_w.map{|k,v|
           if(k == sm.id)
-            d4 = v
-            @report[sm][10] = d4
+            hand_over_loan_weekly = v
+            @report[sm][10] = hand_over_loan_weekly
           end
         }
         
-        d5 = closed_loan_count_current_month(sm,loan_frequency,from_date, to_date)
+        closed_loan_count = closed_loan_count_current_month(sm, loan_frequency, from_date, to_date)
         
-        d6 =  death_cases_current_month(sm,loan_frequency,from_date, to_date)
+        death_cases =  death_cases_current_month(sm, loan_frequency, from_date, to_date)
 
         @taken_over_m.map{|k,v| 
           if (k == sm.id)
-            d15 = v
-            @report[sm][15] = d15
+            taken_over_loan_monthly = v
+            @report[sm][15] = taken_over_loan_monthly
           end
         }
         @hand_over_m.map{|k,v|
           if(k == sm.id)
-            d17 = v
-            @report[sm][17] = d17
+            hand_over_loan_monthly = v
+            @report[sm][17] = hand_over_loan_monthly
           end
         }
         if loan_frequency == INSTALLMENT_FREQUENCIES.index(:weekly)+1
-          @report[sm][7] = d1
-          @report[sm][9] = d3 
-          @report[sm][11] = d5
-          @report[sm][12] = d6
+          @report[sm][7] = loan_count_per_cm_by_frequency
+          @report[sm][9] = pre_month_loan_count_per_cm_by_frequency
+          @report[sm][11] = closed_loan_count
+          @report[sm][12] = death_cases
           
-          @report[sm][13] = @report[sm][22] = ((d1[0]+d3[0]+d2[0]) - (d5[0]+d6[0]+d4[0])).abs
+          @report[sm][13] = @report[sm][22] = ((loan_count_per_cm_by_frequency[0] + pre_month_loan_count_per_cm_by_frequency[0] + taken_over_loan_weekly[0]) - (closed_loan_count[0] + death_cases[0] + hand_over_loan_weekly[0])).abs
         else
-          @report[sm][14] = d1
-          @report[sm][16] = d3
-          @report[sm][18] = d5
-          @report[sm][19] = d6
+          @report[sm][14] = loan_count_per_cm_by_frequency
+          @report[sm][16] = pre_month_loan_count_per_cm_by_frequency
+          @report[sm][18] = closed_loan_count
+          @report[sm][19] = death_cases
           
-          @report[sm][20] = @report[sm][23] = ((d1[0]+d3[0]+d15[0]) - (d5[0]+d6[0]+d17[0])).abs
+           @report[sm][20] = @report[sm][23] = ((loan_count_per_cm_by_frequency[0] + pre_month_loan_count_per_cm_by_frequency[0] + taken_over_loan_monthly[0]) - (closed_loan_count[0] + death_cases[0] + hand_over_loan_monthly[0])).abs
         end
       }
     }
