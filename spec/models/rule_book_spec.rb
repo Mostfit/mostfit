1b2135aaaf5e801d53fa9401ba96b00f4dbb08ca
Ravi Shankar <ravioshankar@gmail.com>
2010-12-23 18:43:15 +0530

    some new tests are added for accounting rules testings

--- spec/models/rule_book_spec.rb
+++ spec/models/rule_book_spec.rb
@@ -33,9 +33,12 @@ describe RuleBook do
     @rule_book = RuleBook.new(:name => "principal repayment", :action => 'principal', :branch => @branch, :fee_id => 0)
     @rule_book.credit_account_rules << CreditAccountRule.create(:credit_account => @credit_account, :percentage => 100)
     @rule_book.debit_account_rules  << DebitAccountRule.create(:debit_account => @debit_account,  :percentage => 100)
-    @rule_book.should be_valid
+    @rule_book.from_date = "2010.1.1"
+    @rule_book.to_date = "2010.12.1"
+    @rule_book.created_by_user_id = 1
     @rule_book.save
-
+    @rule_book.errors.each{|e| puts e}
+    @rule_book.should be_valid
   end
   
   it "should not be valid if name is less than 3 character" do
@@ -53,9 +56,40 @@ describe RuleBook do
     @rule_book_2 = RuleBook.new(:name => "principal repayment", :action => 'principal',:branch =>@branch)
     @rule_book.credit_account_rules << CreditAccountRule.create(:credit_account => @credit_account, :percentage => 100)
     @rule_book.debit_account_rules  << DebitAccountRule.create(:debit_account => @debit_account,  :percentage => 100)
+    @rule_book_2.created_by_user_id = 1
     @rule_book_2.save
-    @rule_book_2.errors
+    @rule_book_2.errors.each{|e| puts e}
     @rule_book_2.should_not be_valid 
   end
+  
+  it "should update old rule if same new rule is been created" do
+    
+    @rule_book_1 = RuleBook.new(:name => "principal repayment1", :action => 'principal', :branch => @branch, :fee_id => 0)
+    @rule_book_1.credit_account_rules << CreditAccountRule.create(:credit_account => @credit_account, :percentage => 100)
+    @rule_book_1.debit_account_rules  << DebitAccountRule.create(:debit_account => @debit_account,  :percentage => 100)
+    @rule_book_1.from_date = "2010.10.1"
+    @rule_book_1.to_date = "2010.12.1"
+    @rule_book_1.created_by_user_id = 1
+    @rule_book_1.save
+    @rule_book_1.errors.each{|e| puts e}
+    @rule_book_1.should be_valid
+    @rule_book.to_date = "2010.9.30"
+    @rule_book.active = false
+    @rule_book.save
+    @rule_book.should be_valid
+  end
 
+  it "should not create rule if from_date is greater than to_date" do
+
+    @rule_book3 = RuleBook.new(:name => "principal repayment test", :action => 'principal', :branch => @branch, :fee_id => 0)
+    @rule_book3.credit_account_rules << CreditAccountRule.create(:credit_account => @credit_account, :percentage => 100)
+    @rule_book3.debit_account_rules  << DebitAccountRule.create(:debit_account => @debit_account,  :percentage => 100)
+    @rule_book3.from_date = "2010.12.1" 
+    @rule_book3.to_date = "2010.1.1"
+    @rule_book3.created_by_user_id = 1
+    @rule_book3.save
+    @rule_book3.errors.each{|e| puts e}
+    @rule_book3.should_not be_valid
+  end
+  
 end
