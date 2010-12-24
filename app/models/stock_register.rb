069bfe3b70a55c74f6ba62afcd15bed9e82d7eb5
Janmejay Rai <janmejay.rai@intellecap.com>
2010-12-24 16:20:18 +0530

    minor change in stock register property

--- app/models/stock_register.rb
+++ app/models/stock_register.rb
@@ -6,7 +6,7 @@ class StockRegister
   property :name,             String,    :length => 100,  :nullable => true,  :index => true
   property :stock_code,       String,    :length => 10,   :nullable => false,  :index => true
   property :stock_name,       String,    :length => 100,  :nullable => true,   :index => true
-  property :stock_quatity,    Integer,                    :nullable => true
+  property :stock_quantity,   Integer,                    :nullable => true
   property :bill_number,      String,                     :nullable => false,  :index => true
   property :bill_date,        Date
   property :date_of_entry,    Date,      :default => Date.today, :nullable => false
