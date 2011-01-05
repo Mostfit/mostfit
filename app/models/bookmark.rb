a223ff7ef2eb728bd331e889e1105f9f655b490d
Piyush Ranjan <piyush.pr@gmail.com>
2011-01-05 15:34:22 +0530

    Removed difference between userrole list of User and Bookmark

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,7 +2,7 @@ class Bookmark
   include DataMapper::Resource
   Types   = [:custom_reports, :other]
   Methods = [:get, :post, :put, :delete]
-  ShareWith = [:none, :all, :admin, :staff_member, :mis_manager, :data_entry, :read_only]
+  #ShareWith = [:none, :all, :admin, :staff_member, :mis_manager, :data_entry, :read_only]
 
   property :id, Serial
   property :name,       String, :length => 50, :nullable => false
@@ -12,7 +12,7 @@ class Bookmark
   property :method,     Enum.send('[]', *Methods), :nullable => false, :default => :get, :index => true
   property :params,     Text,    :nullable => true
   property :user_id,    Integer, :nullable => false, :index => true
-  property :share_with, Flag.send('[]', *ShareWith), :nullable => false, :default => :none, :index => true
+  property :share_with, Flag.send('[]', *User::ROLES), :nullable => false, :default => :none, :index => true
   belongs_to :user
   
   def self.for(user, type=:other)
