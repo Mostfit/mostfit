62b8a28693eede35dad21131804b5dd93071ec3c
Piyush Ranjan <piyush.pr@gmail.com>
2011-01-11 01:43:06 +0530

    Fixed: #582: Client cannot have inactive reason if not inactive

--- app/models/client.rb
+++ app/models/client.rb
@@ -117,6 +117,7 @@ class Client
   validates_with_method  :verified_by_user_id,          :method => :verified_cannot_be_deleted, :if => Proc.new{|x| x.deleted_at != nil}
   validates_attachment_thumbnails :picture
   validates_with_method :date_joined, :method => :dates_make_sense
+  validates_with_method :inactive_reason, :method => :cannot_have_inactive_reason_if_active
 
   def self.from_csv(row, headers)
     if center_attr = row[headers[:center]].strip
@@ -359,6 +360,10 @@ class Client
                                 })
      end
    end
+   
+   def cannot_have_inactive_reason_if_active
+     return [false, "cannot have a inactive reason if active"] if self.active and not inactive_reason.blank?
+     return true
+   end
+
  end
- 
- 
