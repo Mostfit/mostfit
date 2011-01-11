43d091143a3712a90981fdeb1b9774402d0b2772
Piyush Ranjan <piyush.pr@gmail.com>
2011-01-11 18:20:18 +0530

    Fixed pie graphs and figures in lakhs

--- lib/grapher.rb
+++ lib/grapher.rb
@@ -54,11 +54,19 @@ module Grapher
       @y_axis.smoothen(@elements[:values]) if @y_axis and @elements[:values].length>0
       hash = {:title => (@title ? @title.generate : ""), :bg_colour => "#FFFFFF"}
       # change in lakh!
-      if figures_in_lakhs? and @elements[:values].length>0 and @elements[:values].max > 1_00_000
-        @elements[:values] = @y_axis.change_to_lakh(@elements[:values]) if @y_axis
+      if figures_in_lakhs? 
         y_legend = Title.new("figures in lakhs")
         y_legend.style = '{color: #ff4500; font-size: 14px; font-weight: bold}'
-        hash[:y_legend] = y_legend.generate
+        
+        if (@elements[:values].length>0 and @elements[:values].first.is_a?(Numeric) and @elements[:values].max > 1_00_000)
+          @elements[:values] = @y_axis.change_to_lakh(@elements[:values]) if @y_axis
+          hash[:y_legend] = y_legend.generate
+        elsif @elements[:values].map{|x| x[:value]}.max > 1_00_000
+          @elements[:values].each{|ele| 
+            ele[:value] = @y_axis.change_to_lakh(ele[:value])
+          } if @y_axis
+          hash[:y_legend] = y_legend.generate          
+        end
       end
 
       hash += {:elements => [@elements], :x_axis => (@x_axis ? @x_axis.generate : ""), :y_axis => (@y_axis ? @y_axis.generate : "")}
