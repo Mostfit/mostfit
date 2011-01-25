b4c811458165a535f1974494ede37e153bf820ea
Piyush Ranjan <piyush.pr@gmail.com>
2011-01-25 19:35:17 +0530

    Fixed get_selects

--- app/models/loan_history.rb
+++ app/models/loan_history.rb
@@ -619,7 +619,7 @@ class LoanHistory
 
   def self.get_selects(group_by_str, selects)
     selects = selects.map { |sel|
-      sel.to_s
+      "lh.#{sel}"
     }.join(", ") if selects.is_a?(Array)
     selects  = (selects + ", ") if selects and selects.length > 0
     selects += (group_by_str.length>0 ? group_by_str.gsub("GROUP BY", "") : "1")
