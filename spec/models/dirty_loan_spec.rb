01aa44877708ec05724dd24b89394f345cb55cfb
Piyush Ranjan <piyush.pr@gmail.com>
2011-02-15 02:15:06 +0530

    dirty loan spec added

--- spec/models/dirty_loan_spec.rb
+++ spec/models/dirty_loan_spec.rb
@@ -1,7 +1,25 @@
 require File.join( File.dirname(__FILE__), '..', "spec_helper" )
 
 describe DirtyLoan do
+  before(:all) do
+    load_fixtures :staff_members, :branches, :centers, :client_types, :clients, :funders, :funding_lines, :loan_products, :loans
+  end
 
-  it "should have specs"
+  it "should dirty the loan when added" do
+    loan = Loan.first
+    DirtyLoan.pending.length.should == 0
+    DirtyLoan.add(loan)
+    DirtyLoan.pending.length.should == 1
+  end
+
+  it "should clean dirty loan queue when cleared" do
+    loan = Loan.first
+    DirtyLoan.clear
+    DirtyLoan.pending.length.should == 0
+    DirtyLoan.add(loan)
+    DirtyLoan.pending.length.should == 1
+    DirtyLoan.clear
+    DirtyLoan.pending.length.should == 0
+  end
 
 end
