0003742dc95530fdd3b8faf595c6cbea1595d08f
Piyush Ranjan <piyush.pr@gmail.com>
2011-02-15 02:01:00 +0530

    Removing unused CenterHoliday model and one more spec

--- app/models/center_holiday.rb
+++
@@ -1,9 +0,0 @@
-class CenterHoliday
-  include DataMapper::Resource
-  
-  property :id, Serial
-
-  belongs_to :holiday
-  belongs_to :center
-
-end
--- spec/models/predicate_spec.rb
+++
@@ -1,7 +0,0 @@
-require File.join( File.dirname(__FILE__), '..', "spec_helper" )
-
-describe Predicate do
-
-  it "should have specs"
-
-end
