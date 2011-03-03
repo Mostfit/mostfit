874b9848428f1b4a061d01828d3447112d7e04e7
krishnan <krishnan@krishnan-netbook.(none)>
2011-03-04 04:38:16 +0530

    Bank book added, fixes #533

---
+++ app/models/reports/bank_book.rb
@@ -0,0 +1,24 @@
+class BankBook < DayBook
+
+  def generate
+    day_entries = super
+    bank_entries = {}
+    day_entries.each do |branch, account_change|
+      bank_entries[branch] = {} if bank_entries[branch].nil?
+      account_change.each do |account, change|
+        bank_entries[branch][account] = change if account.is_bank_account?
+      end
+    end
+    bank_entries.delete_if {|key, value| bank_entries[key].empty? }
+    bank_entries.sort
+  end
+
+  def name
+    "Bank book for #{date}"
+  end
+
+  def self.name
+    "Bank book"
+  end
+  
+end
