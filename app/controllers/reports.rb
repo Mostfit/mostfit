dbd7236d31687ceb0298e5c5e47638c48183d9e7
Siddharth <svs@intellecap.net>
2011-03-26 13:27:23 +0530

    Bank book report

--- app/controllers/reports.rb
+++ app/controllers/reports.rb
@@ -20,6 +20,7 @@ class Reports < Application
   end
 
   def show(report_type, id)
+    debugger
     provides :pdf
     report_type = params[:report_type] if report_type == "show" and params.key?(:report_type)
     klass = Kernel.const_get(report_type)
