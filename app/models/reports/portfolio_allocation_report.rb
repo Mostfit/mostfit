0bd707fe2e6b3572a22e0e8ae411098fb3bc6e3a
jay3126 <janmejay.rai@intellecap.com>
2011-04-05 23:14:33 +0530

    Portfolio Allocation Report beta version. bug fixed

--- app/models/reports/portfolio_allocation_report.rb
+++ app/models/reports/portfolio_allocation_report.rb
@@ -17,13 +17,13 @@ class PortfolioAllocationReport < Report
   def generate
     data = {}
     funders = {}
-    Funder.all.each{|f| funders[f.id]=(f.portfolios.aggregate(:id, :name, :funder_id, :start_value, :outstanding_value, :created_at)\
-                                       .map{|y| [y[0], y[1], y[2], y[3], y[4], y[5]]})}
+    Funder.all.each{|f| funders[f.id]=(f.portfolios.aggregate(:id, :name, :funder_id, :start_value,
+                                                              :outstanding_value, :created_at).map{|y| [y[0], y[1], y[2], y[3], y[4], y[5]]})}
     
     if @funder
       data[@funder] = funders[@funder.id]
     else
-      Funders.all.each do |x|
+      Funder.all.each do |x|
 	data[x] = funders[x.id]
       end
     end
