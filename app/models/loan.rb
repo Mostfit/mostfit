475cdab848a5d062583176cfe25886829c3b2672
Piyush Ranjan <piyush.pr@gmail.com>
2011-04-08 15:22:00 +0530

    Fixed loan approved nill bug #718

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -976,7 +976,7 @@ class Loan
 
   def convert_blank_to_nil
     self.attributes.each{|k, v|
-      if v.is_a?(String) and v.empty? and self.class.send(k).type==Integer
+      if v.is_a?(String) and v.empty? and self.is_a?(Numeric)
         self.send("#{k}=", nil)
       end
     }
