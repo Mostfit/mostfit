41e3eaee3d8643eb671458285b8dc8af9151579a
Piyush Ranjan <piyush.pr@gmail.com>
2011-04-08 16:01:14 +0530

    removed stray debuggers

--- app/controllers/loans.rb
+++ app/controllers/loans.rb
@@ -107,7 +107,6 @@ class Loans < Application
   end
 
   def update(id)
-    debugger
     klass, attrs = get_loan_and_attrs
     attrs[:interest_rate] = attrs[:interest_rate].to_f / 100 if attrs[:interest_rate].to_f > 0
     attrs[:occupation_id] = nil if attrs[:occupation_id] == ''
@@ -388,7 +387,6 @@ class Loans < Application
   # the loan is not of type Loan of a derived type, therefor we cannot just assume its name..
   # this method gets the loans type from a hidden field value and uses that to get the attrs
   def get_loan_and_attrs   # FIXME: this is a code dup with data_entry/loans
-    debugger
     loan_product = LoanProduct.get(params[:loan_product_id])
     attrs = params[loan_product.loan_type.snake_case.to_sym]
     attrs[:client_id] = params[:client_id] if params[:client_id]
--- app/models/reports/bank_book.rb
+++ app/models/reports/bank_book.rb
@@ -3,7 +3,6 @@ class BankBook < Report
   attr_accessor :from_date, :to_date, :branch_id
 
   def initialize(params, dates, user)
-    debugger
     @from_date = (dates and dates[:from_date]) ? dates[:from_date] : Account.all(:account_category => "Bank").map{|a| a.account_earliest_date}.compact.min
     @to_date   = (dates and dates[:to_date]) ? dates[:to_date] : Date.today
     @name   = "Bank Book from #{@from_date} to #{@to_date}"
@@ -12,7 +11,6 @@ class BankBook < Report
 
 
   def generate
-    debugger
     @data = Account.all(:account_category => "Bank", :branch => @branch)
   end
 
