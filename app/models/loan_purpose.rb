33944007ae6d14e1bd19212a9ac14fe34695ebdd
Piyush Ranjan <piyush.pr@gmail.com>
2011-04-13 15:39:18 +0530

    specs for insurance company and purpose

--- app/models/loan_purpose.rb
+++ app/models/loan_purpose.rb
@@ -5,5 +5,7 @@ class LoanPurpose
   property :value, String
   property :code, String, :length => 3
 
+  validates_is_unique :value
+  validates_is_unique :code
   default_scope(:default).update(:order => [:value])
 end
--- spec/models/insurance_company_spec.rb
+++ spec/models/insurance_company_spec.rb
@@ -2,6 +2,10 @@ require File.join( File.dirname(__FILE__), '..', "spec_helper" )
 
 describe InsuranceCompany do
 
-  it "should have specs"
+  it "should not allow duplicate names" do
+    i1 = InsuranceCompany.create(:name => "foo")
+    i2 = InsuranceCompany.new(:name => "foo")
+    i2.should_not be_valid
+  end
 
 end
