fc0d6932d5f196fdaa0f0e42ca847814a08a2d81
Anurag Priyam <anurag08priyam@gmail.com>
2011-05-13 22:03:12 +0530

    rules: refactor Rule#apply_rule a little
    Since I had to touch Rule#apply_rule anyways (previous commit), I just
    made a small change to how the rules hash is built. Does not seem to
    break anything (passes the rule specs), and reads better :).

--- app/models/rule.rb
+++ app/models/rule.rb
@@ -51,10 +51,8 @@ class Rule
     # can't go ahead without a condition
     return false unless condition
 
-    #puts "Applying Rule #{@name}"
-    h = {:name => @name, :on_action => @on_action, :model_name => @model_name, 
-	    :permit => @permit, :condition => @condition, :precondition => @precondition,
-            :active => @active}
+    # all attributes except id need to be in rules hash
+    h = attributes.tap{|attr| attr.delete(:id)}
 
     # If apply_rule raises a runtime error, we simply cause the validation to
     # fail (return 'false').
