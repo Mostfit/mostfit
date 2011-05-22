2180b272f382b4273b71aba2ab228d65a7b76386
krishnan <krishnan@krishnan-netbook.(none)>
2011-05-22 08:50:32 +0530

    Now displays accounts and sub-totals under each income head for income statement

--- app/models/reports/income_statement.rb
+++ app/models/reports/income_statement.rb
@@ -20,15 +20,15 @@ class IncomeStatement < Report
     accounting_period = AccountingPeriod.get(period) if period
     return data unless accounting_period
     INCOME_HEADS.each do |income_head|
-      total_change = 0.0
-      Account.all(:income_head => income_head, :branch_id => branch_id).inject(total_change) do |total_change, account|
-        net_change = account.change_closing_over_opening_balance_for_period accounting_period.begin_date
-        total_change += net_change if net_change
+      accounts_and_amounts = {}
+      Account.all(:income_head => income_head, :branch_id => branch_id).each do |account|
+        amount = account.change_closing_over_opening_balance_for_period accounting_period.begin_date
+        amount ||= 0.0
+        accounts_and_amounts[account] = amount 
       end
-      data[income_head] = total_change
+      data[income_head] = accounts_and_amounts
     end
     data
-  # TODO: Display by individual accounts under each income head
   # TODO: Display side-by-side for chosen period and previous period
   end
 
