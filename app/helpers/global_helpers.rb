72c29e8ccb71a07e6b0ff80387b626c35e12688c
kiran <kiran@kiran-laptop.(none)>
2011-06-28 17:56:19 +0530

    Made I18n changes for breadcrum

--- app/helpers/global_helpers.rb
+++ app/helpers/global_helpers.rb
@@ -216,21 +216,21 @@ module Merb
       HTML
     end
 
-    def breadcrums
-      # breadcrums use the request.uri and the instance vars of the parent
+    def breadcrumbs
+      # breadcrumbs use the request.uri and the instance vars of the parent
       # resources (@branch, @center) that are available -- so no db queries
       crums, url = [], ''
       request.uri.split("?")[0][1..-1].split('/').each_with_index do |part, index|
         url  << '/' + part
         if part.to_i.to_s.length == part.length  # true when a number (id)
           o = instance_variable_get('@'+url.split('/')[-2].singular)  # get the object (@branch)
-          s = (o.respond_to?(:name) ? link_to(o.name, url) : link_to('#'+o.object_id.to_s, url))
+          s = (o.respond_to?(:name) ? link_to(I18n.t("breadcrumb.#{o.name}", :default => o.name), url) : link_to('#'+o.object_id.to_s, url))
           crums[-1] += ": <b><i>#{s}</i></b>"  # merge the instance names (or numbers)
         else  # when not a number (id)
-          crums << link_to(part.gsub('_', ' '), url)  # add the resource name
+          crums << link_to(I18n.t("breadcrumb.#{part}", :default => part.gsub('_', ' ')), url)  # add the resource name
         end
       end
-      ['You are here', crums].join('&nbsp;<b>&gt;&gt;</b>&nbsp;')  # fancy separator
+      [I18n.t("breadcrumb.start", :default => 'You are here'), crums].join('&nbsp;<b>&gt;&gt;</b>&nbsp;')  # fancy separator
     end
 
     def format_currency(i)
