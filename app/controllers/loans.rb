5cbbb1572b09ea98a541422898482a883aaa9acd
jay3126 <janmejay.rai@intellecap.com>
2011-06-28 15:57:09 +0530

    rolled back changes with respect to commit f7705baaa5c7a56f779c12793090a29ae924ae28

--- app/controllers/loans.rb
+++ app/controllers/loans.rb
@@ -40,37 +40,17 @@ class Loans < Application
     attrs[:interest_rate] = attrs[:interest_rate].to_f / 100 if attrs[:interest_rate].to_f > 0
     @loan_product = LoanProduct.is_valid(params[:loan_product_id])
     raise BadRequest unless @loan_product
-    fee_params = params.delete(:fees)
     @loan = klass.new(attrs)
     @loan.loan_product = @loan_product
-    msg = {}
     if @loan.save
-      msg[:notice] = "Loan '#{@loan.id}' was successfully created"
-      params[:fees].keys.each do |fee_id|
-        @app_fee = []
-        @fee = Fee.get(fee_id)
-        if @fee
-          payable_models ||= Fee::PAYABLE.map{|m| [m[0], [m[1], m[2]]]}.to_hash
-          method = payable_models[@fee.payable_on][1] if @fee
-          if params.include?(:fee_date)
-            if params[:fee_date].empty?
-              date = (@loan.insurance_policy.send(method) if @loan.insurance_policy.respond_to?(method))
-            else
-              date = (Date.strptime(params[:fee_date], Mfi.first.date_format))
-            end
-          end
-          if date
-            @app_fees << ApplicableFee.new(:applicable_on => date, :amount => params[:insurance_policy][:premium], :fee => @fee,
-                                           :applicable_id => @loan.id, :applicable_type => 'Loan')
-          end
-        end
-        result = @app_fees.map{|f| f.save}
-        msg[:error] =  "However, #{result.select{|r| false}.count} insurance premium could not be applied as a fee" if result.include?[false]
+      if params[:return]
+        redirect(params[:return], :message => {:notice => "Loan '#{@loan.id}' was successfully created"})
+      else
+        redirect resource(@branch, @center, @client), :message => {:notice => "Loan '#{@loan.id}' was successfully created"}
       end
-      redirect(params[:return] || resource(@branch, @center, @client), :message => msg)
     else
       set_insurance_policy(@loan_product)
-      @loan.interest_rate *= 100 if @loan.interest_rate
+      @loan.interest_rate *= 100
       render :new # error messages will be shown
     end
   end
