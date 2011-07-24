47de7336ec272b06ebb13ca505c606a3b94cd4a3
Siddharth <svs@intellecap.net>
2011-07-24 22:50:05 +0530

    small accounting ui fixes

--- app/models/account.rb
+++ app/models/account.rb
@@ -13,6 +13,8 @@ class Account
   property :account_category,       Enum.send('[]', *['', 'Cash', 'Bank']), :default => '', :nullable => true, :index => true
   property :asset_class,            Enum.send('[]', *(['', ASSET_CLASSES].flatten)), :default => '', :nullable => true, :index => true
   property :income_head,           Enum.send('[]', *(['', INCOME_HEADS].flatten)), :default => '', :nullable => true, :index => true
+  property :created_at,            DateTime
+  property :deleted_at,            DateTime
 
   belongs_to :account, :model => 'Account', :child_key => [:parent_id]
   belongs_to :account_type
