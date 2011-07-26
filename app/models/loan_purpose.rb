4e65a4e21cec16efdbe247902dc99579886bfcdf
Prasad <prasad@krishnan-desktop.(none)>
2011-07-26 11:36:04 +0530

    renamed the property :value to :name added property parent_id added validation for presence of name added validation for uniqueness of name added relationship to loans n self added default scope

--- app/models/loan_purpose.rb
+++ app/models/loan_purpose.rb
@@ -2,10 +2,16 @@ class LoanPurpose
   include DataMapper::Resource
 
   property :id, Serial
-  property :value, String
+  property :name, String
   property :code, String, :length => 3
+  property :parent_id, Integer, :default => 0
 
-  validates_is_unique :value
+  validates_present :name
+  validates_is_unique :name
   validates_is_unique :code
-  default_scope(:default).update(:order => [:value])
+  
+  has n, :loans
+  has n, :purposes, self, :child_key => [:parent_id]
+  
+  default_scope(:default).update(:order => [:name])
 end
