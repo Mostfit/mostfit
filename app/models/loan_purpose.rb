02fddd38e1f4decc6b07c0251aebe29c5d9e3332
Prasad <prasad@krishnan-desktop.(none)>
2011-07-27 12:12:13 +0530

    enabled I18n for occupation and loan purposes views. ticket#936

--- app/models/loan_purpose.rb
+++ app/models/loan_purpose.rb
@@ -9,9 +9,24 @@ class LoanPurpose
   validates_present :name
   validates_is_unique :name
   validates_is_unique :code
-  
+
   has n, :loans
   has n, :purposes, self, :child_key => [:parent_id]
-  
+
   default_scope(:default).update(:order => [:name])
+
+  validates_with_method :parent_id, :method => :child_cannot_be_parent
+
+  def child_cannot_be_parent
+    if parent_id == 0
+      return true
+    else
+      pid = LoanPurpose.get(parent_id).parent_id
+      if self.id == pid
+        return [false, "Cannot be assigned as parent."]
+      else
+        return true
+      end
+    end
+  end
 end
