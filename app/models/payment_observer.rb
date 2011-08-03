732fffb79466ddec413b475457aeb2c1ae794d90
Koustubh Sinkar <ksinkar@gmail.com>
2011-08-03 16:17:08 +0530

    Transaction Log model added to Mostfit for storing all the accounting related transactions i.e. money exchange transactions that take place in Mostfit

---
+++ app/models/payment_observer.rb
@@ -0,0 +1,65 @@
+class PaymentObserver
+  include DataMapper::Observer
+  
+  observe Payment
+
+  # This function will make entries to the transaction database when save, update or delete event is triggered
+  def self.make_transaction_entry(payment, action)
+
+    client_name = payment.client ? payment.client.name : nil
+    center = payment.client && payment.client.center ? payment.client.center : nil
+    center_id = center ? center.id : nil
+    center_name = center ? center.name : nil
+    
+    staff_member = payment.received_by_staff_id ? StaffMember.get(payment.received_by_staff_id) : nil
+    staff_member_name = staff_member ? staff_member.name : nil
+    fee_name = payment.fee ? payment.fee.name : nil
+        
+    transaction = TransactionLog.create(
+      :txn_id => payment.id,
+      :txn_guid => nil,
+      :txn_update_type => action,
+      :txn_type => :receipt,
+      :nature_of_transaction => "#{payment.type}_received".to_sym,
+      :txn_sub_type_id => payment.fee_id,
+      :txn_sub_type_name => fee_name,
+      :txn_amount => payment.amount,
+      :txn_currency => :INR,
+      :txn_effective_date => payment.received_on,
+      :txn_record_date => payment.created_at,
+      :txn_updated_at_time => nil,
+      :txn_verifed_at_time => nil,
+      :txn_deleted_at_time => nil,
+      :txn_paid_by_type => :client,
+      :txn_paid_by_id => payment.client_id,
+      :txn_paid_by_name => client_name,
+      :txn_received_by_type => :staff_member,
+      :txn_received_by_id => payment.received_by_staff_id,
+      :txn_received_by_name => staff_member_name,
+      :txn_transacted_at_type => :center,
+      :txn_transacted_at_id => center_id,
+      :txn_transacted_at_name => center_name
+    )
+  end
+
+  # We are only observing the create and update methods because the payment model only uses these methods. 
+  # In case of of a payment getting deleted the update function is called and then the save function is called. 
+  
+  # In case of a payment getting updated (i.e. some of the details of the payment are changed and then saved) then the current payment is deleted and a new copy with the updated details is saved with a new payment id
+
+  after :create do
+    PaymentObserver.make_transaction_entry(self, :create)
+  end
+
+  after :update do
+    PaymentObserver.make_transaction_entry(self, :delete)
+  end
+end
+
+
+
+
+
+
+
+
---
+++ app/models/transaction_log.rb
@@ -0,0 +1,41 @@
+class TransactionLog
+  include DataMapper::Resource
+
+  UPDATE_TYPES = [:create, :delete]
+  TRANSACTION_TYPE = [:receipt]
+  NATURE_OF_TRANSACTION = [:principal_received, :interest_received, :fees_received]
+  PAYOR_TYPES = [:client]
+  PAYEE_TYPES = [:staff_member]
+  LOCATION_TYPES = [:center]
+
+  CURRENCY = [:INR, :KES]
+  
+  property :id,                    Serial
+  property :txn_id,                Integer
+  property :txn_guid,              Integer
+  property :txn_update_type,       Enum.send('[]', *UPDATE_TYPES)
+  property :txn_type,              Enum.send('[]', *TRANSACTION_TYPE)
+  property :nature_of_transaction, Enum.send('[]', *NATURE_OF_TRANSACTION)
+  property :txn_sub_type_id,       Integer
+  property :txn_sub_type_name,     String
+  
+  property :txn_amount,          Float
+  property :txn_currency,        Enum.send('[]', *CURRENCY)
+  property :txn_effective_date,  Date
+  property :txn_record_date,     DateTime
+  property :txn_updated_at_time, DateTime
+  property :txn_verifed_at_time, DateTime
+  property :txn_deleted_at_time, DateTime
+  
+  property :txn_paid_by_type,     Enum.send('[]', *PAYOR_TYPES) 
+  property :txn_paid_by_id,       Integer
+  property :txn_paid_by_name,     String
+  
+  property :txn_received_by_type, Enum.send('[]', *PAYEE_TYPES)
+  property :txn_received_by_id,   Integer
+  property :txn_received_by_name, String
+  
+  property :txn_transacted_at_type,       Enum.send('[]', *LOCATION_TYPES)
+  property :txn_transacted_at_id,    Integer
+  property :txn_transacted_at_name,  String
+end
