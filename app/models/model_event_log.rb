b6e0a6e2b32f4f3ad46fa178170936d4ac5c4a3e
krishnanm <km@krishnanm.com>
2011-08-08 12:12:01 +0530

    Minor rename of certain variables to improve clarity

--- app/models/model_event_log.rb
+++ app/models/model_event_log.rb
@@ -1,19 +1,19 @@
 class ModelEventLog
   include DataMapper::Resource
   
-  ACTIONS = [:allow, :disallow]
-  CHANGES = [:create, :update, :destroy]
+  ACCOUNTING_ACTIONS = [:allow, :disallow]
+  MODEL_CHANGES = [:create, :update, :destroy]
   # .to_s.downcase.to_sym
-  OBJECT = [:client, :loan, :loanproduct, :fee, :branch, :funder, :fundingline]
+  OBSERVED_MODELS = [:client, :loan, :loanproduct, :fee, :branch, :funder, :fundingline]
   
   property :id,                                     Serial
   property :event_guid,                             String, :default => lambda{ |obj, p| UUID.generate }
-  property :event_change,                           Enum.send('[]', *CHANGES)
+  property :event_change,                           Enum.send('[]', *MODEL_CHANGES)
   property :event_changed_at,                       DateTime
-  property :event_on_type,                          Enum.send('[]', *OBJECT)
+  property :event_on_type,                          Enum.send('[]', *OBSERVED_MODELS)
   property :event_on_id,                            Integer
   property :event_on_name,                          String
-  property :event_accounting_action,                Enum.send('[]', *ACTIONS)
+  property :event_accounting_action,                Enum.send('[]', *ACCOUNTING_ACTIONS)
   property :event_accounting_action_effective_date, Date
 
 end
