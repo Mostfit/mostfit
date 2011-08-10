619870b94786162b907db136e88def9ba6399b33
Koustubh Sinkar <ksinkar@gmail.com>
2011-08-10 19:21:54 +0530

    Feature #1069: Used a separate transaction_log guid unique to each transaction_log to link extended_info_items to transaction_log rows

--- app/models/transaction_log.rb
+++ app/models/transaction_log.rb
@@ -11,8 +11,10 @@ class TransactionLog
   CURRENCY = [:INR, :KES]
   
   property :id,                    Serial
+  property :txn_log_guid,          String, :default => lambda{ |obj, p| UUID.generate }
   property :txn_id,                Integer
   property :txn_guid,              String
+
   property :txn_update_type,       Enum.send('[]', *UPDATE_TYPES)
   property :txn_type,              Enum.send('[]', *TRANSACTION_TYPE)
   property :nature_of_transaction, Enum.send('[]', *NATURE_OF_TRANSACTION)
@@ -39,5 +41,5 @@ class TransactionLog
   property :txn_transacted_at_id,    Integer
   property :txn_transacted_at_name,  String
 
-  has n, :extended_info_items, :model => 'ExtendedInfoItem'
+  has n, :extended_info_items, :model => 'ExtendedInfoItem', :parent_key => [:txn_log_guid], :child_key => [:parent_guid]
 end
