79cebf013f81faeb2ebd7ddd1048a4f691eb425c
Siddharth <svs@intellecap.net>
2011-08-29 10:52:29 +0530

    fixed trial balance for head office

--- app/models/reports/trial_balance_report.rb
+++ app/models/reports/trial_balance_report.rb
@@ -5,13 +5,13 @@ class TrialBalanceReport < Report
     @from_date = (dates and dates[:from_date]) ? dates[:from_date] : Date.min_date
     @to_date   = (dates and dates[:to_date]) ? dates[:to_date] : Date.today
     @name      = "Trial Balance"
-    @branch_id = params[:branch_id] if params and params.key?(:branch_id) and not params[:branch_id].blank?
+    @branch_id = (params and params.key?(:branch_id) and not params[:branch_id].blank?) ? params[:branch_id] : 0
   #  @page      = params[:page] ||0
     get_parameters(params, user)
   end
 
   def name
-    "Trial Balance for #{((@branch_id and not @branch_id.blank?) ? Branch.get(@branch_id).name : 'Head office')} from #{@from_date} to #{@to_date}"
+    "Trial Balance for #{((@branch_id > 0) ? Branch.get(@branch_id).name : 'Head office')} from #{@from_date} to #{@to_date}"
   end
 
   def self.name
@@ -21,6 +21,7 @@ class TrialBalanceReport < Report
   def generate(param)
     data = {}
     hash = {"journal.date.gte" => @from_date, "journal.date.lte" => @to_date}
+    debugger
     @branch_id = @branch_id.to_i if @branch_id 
     @debit_postings  = Posting.all(hash.merge({:amount.lt => 0})).aggregate(:account_id, :amount.sum).to_hash
     @credit_postings = Posting.all(hash.merge({:amount.gt => 0})).aggregate(:account_id, :amount.sum).to_hash
