01bf301291954d096eb353368545a71e3b2bae47
Siddharth Sharma <svs@intellecap.net>
2011-11-02 02:38:07 +0530

    Admin can now change loan product, client (within a center) and claim settlement date while editing a loan

--- app/controllers/loans.rb
+++ app/controllers/loans.rb
@@ -122,6 +122,7 @@ class Loans < Application
   end
 
   def update(id)
+    debugger
     klass, attrs = get_loan_and_attrs
     attrs[:interest_rate] = attrs[:interest_rate].to_f / 100 if attrs[:interest_rate].to_f > 0
     attrs[:occupation_id] = nil if attrs[:occupation_id] == ''
@@ -510,6 +511,7 @@ class Loans < Application
   # the loan is not of type Loan of a derived type, therefor we cannot just assume its name..
   # this method gets the loans type from a hidden field value and uses that to get the attrs
   def get_loan_and_attrs   # FIXME: this is a code dup with data_entry/loans
+    debugger
     if params[:id] and not params[:id].blank?
       loan =  Loan.get(params[:id])      
       loan_product = loan.loan_product
@@ -517,11 +519,14 @@ class Loans < Application
       klass = loan.class
     else
       loan_product = LoanProduct.get(params[:loan_product_id])
-      attrs = params[loan_product.loan_type_string.snake_case.to_sym] || params[:loan]
+      attrs = (params[loan_product.loan_type_string.snake_case.to_sym] || params[:loan]).dup
       raise NotFound if not params[:loan_type]
       klass = Kernel::const_get(params[:loan_type])
     end
-    attrs[:client_id] = params[:client_id] if params[:client_id]
+    debugger
+    attrs[:client_id] ||= params[:client_id] if params[:client_id]
+    attrs[:client] = Client.get(attrs.delete(:client_id))
+    attrs[:loan_product] = LoanProduct.get(attrs.delete(:loan_product_id)) if attrs[:loan_product_id]
     attrs[:insurance_policy] = params[:insurance_policy] if params[:insurance_policy]
     attrs[:repayment_style_id] ||= loan_product.repayment_style.id
     [klass, attrs]
