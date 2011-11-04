80e79bf41c5f5e213e0d6a234c205726e5a78c20
Siddharth Sharma <svs@intellecap.net>
2011-11-04 15:42:03 +0530

    fixed some annoyances in Rules * added a 'toggle active state' link on rule show page to turn a rule on and off. this was necessitated due to the 'edit' screen mangling the conditions and preconditions. * tweaked data access observer to allow destroy\! in some instances

--- app/models/data_access_observer.rb
+++ app/models/data_access_observer.rb
@@ -47,6 +47,7 @@ class DataAccessObserver
   end
 
   def self.check_session(obj)
+    debugger
     return true if Merb.environment == "test"
     return true if File.writable?("config.ru") and not @_user
     @_user = User.authenticate(ENV['MOSTFIT_USER'], ENV['MOSTFIT_PASSWORD'])    unless @_user
@@ -83,7 +84,7 @@ class DataAccessObserver
   end
   
   before :destroy! do
-    raise NotPrivileged
+    raise NotPrivileged if self.model.properties.map{|r| r.name}.include?(:deleted_at)
   end
 
 end
