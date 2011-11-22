403159eb62445c5e3eb5dcc09bd3d376d95d1198
Siddharth Sharma <svs@intellecap.net>
2011-11-23 00:38:55 +0530

    fixed center meeting day bug. thanks anand. report format columns now orderable

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -462,6 +462,7 @@ class Loan
     end
     
     # take care of date changes in weekly schedules
+    debugger
     if [:weekly, :biweekly, :quadweekly].include?(installment_frequency) and cl=self.client(:fields => [:id, :center_id]) and cen=cl.center and cen.meeting_day != :none and ensure_meeting_day
       unless (new_date.weekday == cen.meeting_day_for(new_date) or (cen.meeting_day_for(new_date) == :none))
         # got wrong val. recalculate
@@ -1049,6 +1050,7 @@ class Loan
   end
   # the installment dates
   def installment_dates
+    debugger
     return @_installment_dates if @_installment_dates
     if self.loan_product.loan_validations and self.loan_product.loan_validations.include?(:scheduled_dates_must_be_center_meeting_days)
       @_installment_dates =  ([scheduled_first_payment_date].concat(client.center.get_meeting_dates(number_of_installments, scheduled_first_payment_date))).uniq
