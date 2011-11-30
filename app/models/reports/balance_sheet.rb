89ccdda9329141bea879849bd58900893033a52f
krishnanm <km@krishnanm.com>
2011-11-30 08:47:50 +0530

    Fixes to balance sheet and income statement

--- app/models/reports/balance_sheet.rb
+++ app/models/reports/balance_sheet.rb
@@ -22,7 +22,8 @@ class BalanceSheet < Report
       accounts_and_balances = {}
       Account.all(:asset_class => asset_class).each do |account|
         closing_balance = account.closing_balance_as_of accounting_period.end_date
-        accounts_and_balances[account] = closing_balance
+        bal = closing_balance || 0.0
+        accounts_and_balances[account] = bal
       end
       data[asset_class] = accounts_and_balances
     end
