361bfd74bfaf75ff6167bdc132074cc7d22733df
Peter Duijnstee <peter@commuun.nl>
2011-12-29 20:35:45 +0100

    Updated stock_register spec

--- spec/models/stock_register_spec.rb
+++ spec/models/stock_register_spec.rb
@@ -1,38 +1,9 @@
 require File.join( File.dirname(__FILE__), '..', "spec_helper" )
 
 describe StockRegister do
-
-  before(:all) do
-    StaffMember.all.destroy!
-    @manager = StaffMember.new(:name => "Mr. Ram Turanai")
-    @manager.save
-    @manager.errors
-    @manager.should be_valid
-  end
-
-  before(:all) do
-    Branch.all.destroy!
-    @branch = Branch.new(:name => "Kanpur")
-    @branch.manager = @manager
-    @branch.code = "branch1"
-    @branch.save
-    @branch.errors.each {|e| p e}
-    @branch.should be_valid
-  end
-
   before(:each) do
     StockRegister.all.destroy!
-    @stock_register = StockRegister.new
-    @stock_register.branch = @branch
-    @stock_register.stock_code = "CHR123"
-    @stock_register.stock_name = "Chair"
-    @stock_register.stock_quantity = 30
-    @stock_register.bill_number = 6754328
-    @stock_register.bill_date = '20-12-2010'
-    @stock_register.date_of_entry = '24-12-2010'
-    @stock_register.manager = @manager
-    @stock_register.save
-    @stock_register.errors {|e| p e}
+    @stock_register = Factory(:stock_register)
     @stock_register.should be_valid
   end
 
@@ -42,8 +13,8 @@ describe StockRegister do
   end
 
   it "should belong to a particular branch" do
-    @stock_register.branch = @branch
-    @stock_register.should be_valid
+    @stock_register.branch = nil
+    @stock_register.should_not be_valid
   end
 
   it "should not be valid without stock code" do
