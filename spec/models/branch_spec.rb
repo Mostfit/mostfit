5ca7a254d2cb015ad050ed7a56268ad013ad1213
Amit Rade <amit@sweetpea.(none)>
2012-02-02 18:02:47 +0530

    add new specs for branch in branch_spech.rb

--- spec/models/branch_spec.rb
+++ spec/models/branch_spec.rb
@@ -27,10 +27,74 @@ describe Branch do
     @branch.name = nil
     @branch.should_not be_valid
   end
- 
+
   it "should not be valid with a name shorter than 3 characters" do
     @branch.name = "ok"
     @branch.should_not be_valid
+  end
+ 
+ it "should valid with a name more than 3 character" do
+  @branch.name = "branch"
+  @branch.should be_valid
+  end
+ 
+  it "should not be valid without a creation_date " do
+    @branch.creation_date= nil
+    @branch.should_not be_valid
+  end
+ 
+  it "should not be valid without a code" do
+   @branch.code= nil
+   @branch.should_not be_valid
+  end
+
+  it "should not be valid with a code greater than 10 character" do
+   @branch.code="1234567890123"
+   @branch.should_not be_valid
+  end
+
+  it "should be valid with code" do
+   @branch.code="ok"
+   @branch.should be_valid
+  end    
+
+  it"should be valid without contact number" do
+   @branch.contact_number= nil
+   @branch.should be_valid
+   end
+
+ it "should not be valid with contact_number grater than 40 character" do
+   @branch.contact_number="12345678901234567890123456789012345678901234567890"
+   @branch.should_not be_valid
+  end
+
+ it "should be valid with contact_number less than 40 character" do
+   @branch.contact_number="1235678901234567890"
+   @branch.should be_valid
+  end
+
+  it"should be valid without landmark name" do
+  @branch.landmark= nil
+  @branch.should be_valid
+ end
+  it"should be valid with a landmark name  less than 100 character" do
+  @branch.landmark="abcdefghijklmnopqrstuvwxyz"
+  @branch.should be_valid
+ end
+
+  it"should  not be valid with a landmark name more  than 100 character" do
+  @branch.landmark="abcdefghijklmnopqrstuvwxyzasdsdsdfsfghgfhgfghfhfghdfsdfgfdgfsfdgfdfsgfdgfdgfcgfdfghdfdgfdghdgfsgcghdgshgdgdfhdghfdghfhdgfdgdgfcgdddhfdghdghhjgjgjfghfhjghfghfgdjgdgfdfdfgfffhggfgfdgfdhgkhfkhjghgljghjftydtrsrfyufgxdfgfcgxdfzgfcgfzdszgfcgfzdfxjghcgfjhgyuflghjfjgdfxgc"
+  @branch.should_not be_valid
+ end
+  
+ it"should not be valid without area_id" do
+  @branch.area_id=nil
+  @branch.should_not be_valid
+ end
+ 
+ it"should be valid with area_id" do
+  @branch.area_id="1"
+  @branch.should be_valid
   end
  
   it "should be able to 'have' centers" do
